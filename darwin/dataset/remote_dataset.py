7a274229b1d789e964c4faf66d71caeebe779d04
Renthal <albertimichele91@hotmail.com>
2019-08-12 15:47:13 +0100

    fixed import

--- darwin/dataset/remote_dataset.py
+++ darwin/dataset/remote_dataset.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import List, Optional
 
 import darwin
+from darwin.dataset import LocalDataset
 from darwin.dataset.download_manager import download_all_images_from_annotations
 from darwin.dataset.upload_manager import _split_on_file_type, upload_file_to_s3
 from darwin.utils import urljoin
