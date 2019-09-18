59c2d2851569389a0f64d2d090c23e3c2c6c7c28
Jon Almazan <jon@v7labs.com>
2019-09-18 18:14:10 +0100

    add fetch_darwin_dataset

--- darwin/pytorch/utils.py
+++ darwin/pytorch/utils.py
@@ -1,7 +1,13 @@
 import os
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 from pycocotools import mask as coco_mask
+from pathlib import Path
+from tqdm import tqdm
+
+from darwin.client import Client
 
 from PIL import Image
 try:
@@ -34,14 +40,6 @@ def load_pil_image(path):
     else:
         raise TypeError(f"unsupported image type {pic.mode}")
     return pic
-
-
-def mkdir(path):
-    try:
-        os.makedirs(path)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
 
 
 def _is_pil_image(img):
@@ -101,6 +99,95 @@ def polygon_area(x, y):
     return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 
+def extract_classes(files):
+    classes = {}
+    idx_to_classes = {}
+    for i, fname in enumerate(files):
+        with open(fname) as f:
+            d = json.load(f)
+            idx_to_classes[i] = []
+            for a in d["annotations"]:
+                cls_name = a["name"]
+                try:
+                    classes[cls_name].add(i)
+                except KeyError:
+                    classes[cls_name] = set([i])
+                if cls_name not in idx_to_classes[i]:
+                    idx_to_classes[i].append(cls_name)
+    return classes, idx_to_classes
+
+
+def fetch_darwin_dataset(db_name, client=None, val_perc=0.1, test_perc=0, image_status="done",
+                         force_resplit=False, split_seed=None, **kwargs):
+    if client is None:
+        client = Client.default()
+
+    # Get data
+    local_datasets = {dataset.slug: dataset for dataset in client.list_local_datasets()}
+    if db_name in local_datasets:
+        dataset = local_datasets[db_name]
+    else:
+        remote_datasets = [dataset.slug for dataset in client.list_remote_datasets()]
+        if db_name in remote_datasets:
+            dataset = client.get_remote_dataset(slug=db_name)
+            progress, _count = dataset.pull(image_status=image_status)
+            with tqdm(total=_count, desc=f"Downloading dataset {db_name}") as pbar:
+                for _ in progress():
+                    pbar.update()
+        else:
+            raise ValueError(f"could not find dataset {db_name}")
+
+    # Find annotations and create folders
+    root = Path(client.project_dir) / db_name
+    annot_path = root / "annotations"
+    annot_files = [f for f in annot_path.glob('*.json')]
+    num_images = len(annot_files)
+    lists_path = root / "lists"
+    if not lists_path.exists():
+        os.makedirs(lists_path)
+
+    # Extract classes
+    fname = lists_path / "classes.txt"
+    if not fname.exists():
+        # Extract list of classes
+        classes, idx_to_classes = extract_classes(annot_files)
+        classes_names = [k for k in classes.keys()]
+        classes_names.insert(0, '__background__')
+        with open(lists_path / 'classes.txt', 'w') as f:
+            for c in classes_names:
+                f.write(f"{c}\n")
+
+    # Create split
+    split_id = f"split_val{val_perc}_test{test_perc}"
+    split_path = lists_path / split_id
+    if not split_path.exists() or force_resplit:
+        os.makedirs(split_path)
+        num_train = int(num_images * (1 - (val_perc + test_perc)))
+        num_test = int(num_images * test_perc)
+        num_val = num_images - num_train - num_test
+
+        indices = np.random.permutation(num_images)
+        train_idx = indices[:num_train]
+        val_idx = indices[num_train:num_train+num_val]
+        test_idx = indices[num_train+num_val:]
+
+        # Write files
+        with open(lists_path / 'train.txt', 'w') as f:
+            for i in train_idx:
+                f.write(f"{annot_files[i].stem}\n")
+        if num_val > 0:
+            with open(lists_path / 'val.txt', 'w') as f:
+                for i in val_idx:
+                    f.write(f"{annot_files[i].stem}\n")
+        if num_test > 0:
+            with open(lists_path / 'test.txt', 'w') as f:
+                for i in test_idx:
+                    f.write(f"{annot_files[i].stem}\n")
+
+    return root, split_id
+
+
+# VISUALIZATION
 def visualize_mask_output(image, masks, classes, colors, threshold=0.5):
     W, H = image.size
     outmask = np.zeros((H, W, 4), dtype=np.uint8)
