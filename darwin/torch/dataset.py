2edfbfb01a7c36b86191e49367adb4dee9a4e0f4
Renthal <albertimichele91@hotmail.com>
2019-10-01 17:06:00 +0100

    followup on PR

--- darwin/torch/dataset.py
+++ darwin/torch/dataset.py
@@ -13,91 +13,82 @@ from darwin.torch.utils import (
     polygon_area,
 )
 
-
 def get_dataset(
-    dataset_name: str,
-    image_set: Optional[str] = "train",
-    mode: Optional[str] = "raw",
-    transforms: Optional = None,
-    poly_to_mask: Optional[bool] = False,
-    client: Optional[Client] = None,
-    **kwargs,
+        dataset_name: str,
+        image_set: Optional[str] = "train",
+        mode: Optional[str] = "raw",
+        transforms: Optional = None,
+        client: Optional[Client] = None,
+        **kwargs,
 ):
     """
     Pulls a dataset from Darwin and returns a Dataset class that can be used with a PyTorch dataloader
 
     Parameters
     ----------
-    dataset_name : TODO type
+    dataset_name : str
         Identifier of the dataset in Darwin
-    image_set :  TODO type
-        Split set [train, val, test]
-    mode :  TODO type
+    image_set : str
+        Split set, values must be either 'train', 'val' or 'test'
+    mode : str
         selects the dataset type [image_classification, instance_segmentation, semantic_segmentation]
-    transforms :  TODO type
+    transforms :  [torchvision.transforms]
         List of PyTorch transforms
-    poly_to_mask :
-         TODO description
-    client:  TODO type
+    client:  Client
         Darwin's client
-
-    TODO these entries are not part of the parameters (in the signature) and do not exist below: remove or fix?
-    val_percentage:  TODO type
-        percentage of images used in the validation set
-    test_percentage:  TODO type
-        percentage of images used in the validation set
-    force_fetching:  TODO type
-        discard local dataset and pull again from Darwin
-    force_resplit:  TODO type
-        discard previous split and create a new one
-    split_seed:  TODO type
-        fix seed for random split creation
 
     Returns
     -------
-    dataset : Dataset
+    Dataset
         Dataset class
     """
 
     root, split_id = fetch_darwin_dataset(dataset_name, client, **kwargs)
 
     if mode == "raw":
-        dataset = Dataset(root, image_set=image_set, split_id=split_id, transforms=transforms)
-    elif mode == "classification":
-        dataset = ClassificationDataset(
-            root, image_set=image_set, split_id=split_id, transforms=transforms
+        return Dataset(
+            root=root, image_set=image_set, split_id=split_id, transforms=transforms
         )
-    elif mode == "instance_segmentation":
+
+    if mode == "classification":
+        return ClassificationDataset(
+            root=root, image_set=image_set, split_id=split_id, transforms=transforms
+        )
+
+    if mode == "instance_segmentation":
         import darwin.torch.transforms as T
-
-        trfs = [T.ConvertPolysToInstanceMasks()]
-        if transforms is not None:
-            trfs.append(transforms)
-        transforms = T.Compose(trfs)
-        dataset = InstanceSegmentationDataset(
-            root, image_set=image_set, split_id=split_id, transforms=transforms
+        if transforms is None:
+            transforms = []
+        transforms.insert(0, T.ConvertPolysToInstanceMasks())
+        transforms = T.Compose(transforms)
+        return InstanceSegmentationDataset(
+            root=root, image_set=image_set, split_id=split_id, transforms=transforms
         )
-    elif mode == "semantic_segmentation":
+
+    if mode == "semantic_segmentation":
         import darwin.torch.transforms as T
-
-        trfs = [T.ConvertPolysToMask()]
-        if transforms is not None:
-            trfs.append(transforms)
-        transforms = T.Compose(trfs)
-        dataset = SemanticSegmentationDataset(
-            root, image_set=image_set, split_id=split_id, transforms=transforms
+        if transforms is None:
+            transforms = []
+        transforms.insert(0, T.ConvertPolysToMask())
+        transforms = T.Compose(transforms)
+        return SemanticSegmentationDataset(
+            root=root, image_set=image_set, split_id=split_id, transforms=transforms
         )
-    else:
-        raise ValueError("Dataset type {mode} not supported.")
 
-    return dataset
+    raise ValueError("Dataset type {mode} not supported.")
 
 
 class Dataset(object):
     def __init__(self, root: Path, image_set: str, split_id: Optional[str] = None, transforms=None):
         self.root = root
-        self.transforms = transforms
         self.image_set = image_set
+        self.transforms = transforms
+        self.images_path = []
+        self.annotations_path = []
+        self.classes = None
+        self.original_images_path = None
+        self.original_annotations_path = None
+        self.original_classes = None
 
         if self.image_set not in ["train", "val", "test"]:
             raise ValueError(f"Unknown partition {self.image_set}")
@@ -111,51 +102,81 @@ class Dataset(object):
             raise FileNotFoundError(
                 "Could not find partition {image_set} in {path_to_lists}. (Is the percentage larger than 0?)"
             )
-        stems = [e.strip() for e in open(file_partition)]
 
-        exts = ["jpg", "jpeg", "png"]
-        self.annotations, self.images = [], []
-        for s in stems:
-            annot_path = self.root / f"annotations/{s}.json"
-            for ext in exts:
-                im_path = self.root / f"images/{s}.{ext}"
-                if im_path.is_file():
-                    self.images.append(im_path)
-                    self.annotations.append(annot_path)
+        extensions = ["jpg", "jpeg", "png"]
+        stems = [e.strip() for e in open(str(file_partition))]
+        for stem in stems:
+            annotation_path = self.root / f"annotations/{stem}.json"
+            for extension in extensions:
+                image_path = self.root / f"images/{stem}.{extension}"
+                if image_path.is_file():
+                    self.images_path.append(image_path)
+                    self.annotations_path.append(annotation_path)
                     break
-        if len(self.images) == 0:
-            raise ValueError(f"could not find any {exts} file in {self.root/'images'}")
+        if len(self.images_path) == 0:
+            raise ValueError(f"could not find any {extensions} file in {self.root/'images'}")
+        if len(self.images_path) != len(self.annotations_path):
+            raise ValueError(f"some annotations ({len(self.annotations_path)}) "
+                             f"do not have a corresponding image ({len(self.images_path)})")
 
-    def __getitem__(self, idx: int):
-        # load images ad masks
-        img = load_pil_image(self.images[idx])
-        target = self._load_annotations_and_remap(idx)
+    def subsample(self, perc: float):
+        num = int(len(self.annotations_path) * perc)
+        #TODO warning this does not protect from x-contamination! See PR
+        indices = random.sample(range(len(self.annotations_path)), num)
+        self.original_images_path = self.images_path
+        self.images_path = [self.images_path[i] for i in indices]
+        self.original_annotations_path = self.annotations_path
+        self.annotations_path = [self.annotations_path[i] for i in indices]
+
+    def extend(self, db, extend_classes=False):
+        if self.classes != db.classes and not extend_classes:
+            raise ValueError(
+                "Operation dataset_a + dataset_b could not be computed: classes should match. \
+                             Use flag extend_classes=True to combine both lists of classes."
+            )
+        self.classes = list(set(self.classes).union(set(db.classes)))
+
+        self.original_images_path = self.images_path
+        self.images_path += db.images
+        self.original_annotations_path = self.annotations_path
+        self.annotations_path += db.annotations
+        return self
+
+    def _map_annotation(self, index: int):
+        """
+        Load an annotation and filter out the extra classes according to what
+        specified in `self.classes`
+
+        Parameters
+        ----------
+        index : int
+            Index of the annotation to read
+
+        Returns
+        -------
+        dict
+        A new dictionary containing the index and the filtered annotation
+        """
+        with open(str(self.annotations_path[index])) as f:
+            annotation = json.load(f)["annotations"]
+
+        if self.classes is None:
+            raise ValueError(f"No classes have been specified yet (self.classes is still none!).")
+
+        # Filter out unused classes
+        annotation = [a for a in annotation if a["name"] in self.classes]
+        return {"image_id": index,
+                "annotations": annotation}
+
+    def __getitem__(self, index: int):
+        # load images and masks
+        img = load_pil_image(self.images_path[index])
+        target = self._map_annotation(index)
 
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
         return img, target
-
-    def _load_annotations_and_remap(self, idx: int):
-        with open(self.annotations[idx]) as f:
-            anno = json.load(f)["annotations"]
-
-        # Filter out unused classes
-        anno = [obj for obj in anno if obj["name"] in self.classes]
-
-        res = dict(image_id=idx, annotations=anno)
-        return res
-
-    def __len__(self):
-        return len(self.images)
-
-    def subsample(self, perc: float):
-        num = int(len(self.annotations) * perc)
-        indices = random.sample(range(len(self.annotations)), num)
-        self.orig_imgs = self.images
-        self.images = [self.images[i] for i in indices]
-        self.orig_annotations = self.annotations
-        self.annotations = [self.annotations[i] for i in indices]
 
     def __add__(self, db):
         if self.classes != db.classes:
@@ -163,38 +184,23 @@ class Dataset(object):
                 "Operation dataset_a + dataset_b could not be computed: classes should match. \
                              Use dataset_a.extend(dataset_b, extend_classes=True) to combine both lists of classes"
             )
-        self.orig_imgs = self.images
-        self.images += db.images
-        self.orig_annotations = self.annotations
-        self.annotations += db.annotations
+        self.original_images_path = self.images_path
+        self.images_path += db.images
+        self.original_annotations_path = self.annotations_path
+        self.annotations_path += db.annotations
         return self
 
-    def extended(self, db, extend_classes=False):
-        if self.classes != db.classes and not extend_classes:
-            raise ValueError(
-                "Operation dataset_a + dataset_b could not be computed: classes should match. \
-                             Use flag extend_classes=True to combine both lists of classes."
-            )
-        elif self.classes != db.classes and extend_classes:
-            self.orig_classes = self.classes
-            for c in db.classes:
-                if c not in self.classes:
-                    self.classes.append(c)
+    def __mul__(self, percentage: float):
+        self.subsample(percentage)
 
-        self.orig_imgs = self.images
-        self.images += db.images
-        self.orig_annotations = self.annotations
-        self.annotations += db.annotations
-        return self
-
-    def __mul__(self, perc: float):
-        self.subsample(perc)
+    def __len__(self):
+        return len(self.images_path)
 
     def __str__(self):
         format_string = (
             f"{self.__class__.__name__}():\n"
             f"  Root: {self.root}\n"
-            f"  Number of images: {len(self.images)}"
+            f"  Number of images: {len(self.images_path)}"
         )
         return format_string
 
@@ -202,24 +208,23 @@ class Dataset(object):
 class ClassificationDataset(Dataset):
     def __init__(self, root: Path, image_set: str, split_id: Optional[str] = None, transforms=None):
         super(ClassificationDataset, self).__init__(root, image_set, split_id, transforms)
-        self.classes = [e.strip() for e in open(root / "lists/classes_tags.txt")]
+        self.classes = [e.strip() for e in open(str(root / "lists/classes_tags.txt"))]
 
-    def _load_annotations_and_remap(self, idx: int):
-        with open(self.annotations[idx]) as f:
-            anno = json.load(f)["annotations"]
-            for obj in anno:
+    def _map_annotation(self, index: int):
+        with open(str(self.annotations_path[index])) as f:
+            annotation = json.load(f)["annotations"]
+            for obj in annotation:
                 if "tag" in obj:
-                    target = {"category_id": self.classes.index(obj["name"])}
-        return target
+                    return {"category_id": self.classes.index(obj["name"])}
 
 
 class InstanceSegmentationDataset(Dataset):
     def __init__(self, root: Path, image_set: str, split_id: Optional[str] = None, transforms=None):
         super(InstanceSegmentationDataset, self).__init__(root, image_set, split_id, transforms)
-        self.classes = [e.strip() for e in open(root / "lists/classes_masks.txt")]
+        self.classes = [e.strip() for e in open(str(root / "lists/classes_masks.txt"))]
 
-    def _load_annotations_and_remap(self, idx: int):
-        with open(self.annotations[idx]) as f:
+    def _map_annotation(self, index: int):
+        with open(str(self.annotations_path[index])) as f:
             anno = json.load(f)["annotations"]
 
         # Filter out unused classes
@@ -227,9 +232,11 @@ class InstanceSegmentationDataset(Dataset):
 
         target = []
         for obj in anno:
-            new_obj = {"image_id": idx, "iscrowd": 0}
-            new_obj["category_id"] = self.classes.index(obj["name"])
-            new_obj["segmentation"] = [convert_polygon_to_sequence(obj["polygon"]["path"])]
+            new_obj = {"image_id": index,
+                       "iscrowd": 0,
+                       "category_id": self.classes.index(obj["name"]),
+                       "segmentation": [convert_polygon_to_sequence(obj["polygon"]["path"])]}
+
             seg = np.array(new_obj["segmentation"][0])
             xcoords = seg[0::2]
             ycoords = seg[1::2]
@@ -238,6 +245,7 @@ class InstanceSegmentationDataset(Dataset):
             w = (np.max(xcoords) - x) + 1
             h = (np.max(ycoords) - y) + 1
             new_obj["bbox"] = [x, y, w, h]
+
             poly_area = polygon_area(xcoords, ycoords)
             bbox_area = w * h
             if poly_area > bbox_area:
@@ -245,32 +253,29 @@ class InstanceSegmentationDataset(Dataset):
                     f"polygon's area should be <= bbox's area. Failed {poly_area} <= {bbox_area}"
                 )
             new_obj["area"] = poly_area
+
             target.append(new_obj)
 
-        res = dict(image_id=idx, annotations=target)
-
-        return res
+        return {'image_id':index,
+                'annotations':target}
 
 
 class SemanticSegmentationDataset(Dataset):
     def __init__(self, root: Path, image_set: str, split_id: Optional[str] = None, transforms=None):
         super(SemanticSegmentationDataset, self).__init__(root, image_set, split_id, transforms)
-        self.classes = [e.strip() for e in open(root / "lists/classes_masks.txt")]
+        self.classes = [e.strip() for e in open(str(root / "lists/classes_masks.txt"))]
         if self.classes[0] == "__background__":
             self.classes = self.classes[1:]
 
-    def _load_annotations_and_remap(self, idx: int):
-        with open(self.annotations[idx]) as f:
-            anno = json.load(f)["annotations"]
+    def _map_annotation(self, index: int):
+        with open(str(self.annotations_path[index])) as f:
+            annotation = json.load(f)["annotations"]
 
         # Filter out unused classes
-        anno = [obj for obj in anno if obj["name"] in self.classes]
+        annotation = [obj for obj in annotation if obj["name"] in self.classes]
 
         target = []
-        for obj in anno:
-            new_obj = {}
-            new_obj["category_id"] = self.classes.index(obj["name"])
-            new_obj["segmentation"] = [convert_polygon_to_sequence(obj["polygon"]["path"])]
-            target.append(new_obj)
-
+        for obj in annotation:
+            target.append({"category_id": self.classes.index(obj["name"]),
+                           "segmentation": [convert_polygon_to_sequence(obj["polygon"]["path"])]})
         return target
