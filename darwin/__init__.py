036ad12f25a4cf856ca694b66f1d6a4e9e4573c9
Renthal <albertimichele91@hotmail.com>
2019-10-09 17:48:14 +0100

    removed as to prevent torch to be imported automatically

--- darwin/__init__.py
+++ darwin/__init__.py
@@ -2,4 +2,3 @@ import darwin.exceptions
 from .client import Client
 from .dataset import Dataset, LocalDataset
 from .team import Team
-from .torch import get_dataset, ClassificationDataset, InstanceSegmentationDataset, SemanticSegmentationDataset
