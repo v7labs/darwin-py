ff0a14c6258d1316e61c205ce37794c82d24ab26
Renthal <albertimichele91@hotmail.com>
2019-10-14 16:03:22 +0100

    fixed by removing get_dataset

--- darwin/torch/__init__.py
+++ darwin/torch/__init__.py
@@ -1,3 +1,3 @@
-from .dataset import get_dataset, ClassificationDataset, InstanceSegmentationDataset, SemanticSegmentationDataset
+from .dataset import ClassificationDataset, InstanceSegmentationDataset, SemanticSegmentationDataset
 
 # Requirements: pytorch, torchvision, pycocotools, sklearn
