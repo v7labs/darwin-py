4e9bfbd582cc21224edda5699cb2825ba502548b
Renthal <albertimichele91@hotmail.com>
2019-10-18 11:52:23 +0100

    unified file name

--- darwin/dataset/utils.py
+++ darwin/dataset/utils.py
@@ -247,9 +247,9 @@ def split_dataset(
         os.makedirs(str(split_path), exist_ok=True)
 
         # RANDOM SPLIT
-        train_path = Path(split_path / "random_split_train.txt")
-        val_path = Path(split_path / "random_split_val.txt")
-        test_path = Path(split_path / "random_split_test.txt")
+        train_path = Path(split_path / "random_train.txt")
+        val_path = Path(split_path / "random_val.txt")
+        test_path = Path(split_path / "random_test.txt")
         splits['random'] = {'train': train_path, 'val':val_path, 'test':test_path}
         # Compute split sizes
         dataset_size = sum(1 for _ in annotation_files)
@@ -269,7 +269,7 @@ def split_dataset(
 
         # STRATIFIED SPLIT ON POLYGONS
         train_path = Path(split_path / "stratified_polygon_train.txt")
-        val_path = Path(split_path / "stratified_polygon_split_val.txt")
+        val_path = Path(split_path / "stratified_polygon_val.txt")
         test_path = Path(split_path / "stratified_polygon_test.txt")
         splits['polygon'] = {'train': train_path, 'val': val_path, 'test': test_path}
         # Stratify
