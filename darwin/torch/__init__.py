2aede1310a841f99dc5e8b97e485f5ffd066f3f2
Jon Almazan <jon.almazan@gmail.com>
2020-05-12 18:03:03 +0100

    import check

--- darwin/torch/__init__.py
+++ darwin/torch/__init__.py
@@ -1,8 +1,19 @@
-# Requirements: pytorch, torchvision, pycocotools, sklearn
+# Requirements: pytorch, torchvision, pycocotools
 from .dataset import (
     get_dataset,
-    ClassificationDataset,
     Dataset,
+    ClassificationDataset,
     InstanceSegmentationDataset,
     SemanticSegmentationDataset,
 )
+
+try:
+    import torch
+    import torchvision
+except ImportError:
+    raise ImportError(f"pytorch and torchvision required. Install it using: pip install torch torchvision")
+
+try:
+    import pycocotools
+except ImportError:
+    raise ImportError(f"pycocotools required. Install it using: pip install cython; pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'")
