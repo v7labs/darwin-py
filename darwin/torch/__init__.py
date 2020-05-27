b5f6225b5696c5bbb556aba2343a8b94e3994025
Jon Almazán <jon.almazan@gmail.com>
2020-05-27 20:32:20 +0100

    Support for local releases (#75)
    * split_dataset function takes a dataset path
    
    * update doc
    
    * add get_classes
    
    * re-format get_annotations
    
    * add documentation
    
    * rename class_type
    
    * add function classes
    
    * refactor class_type
    
    * add split and annotations functions
    
    * move get_annotations to dataset.utils
    
    * create classes in pull() and fix split()
    
    * add image_id
    
    * bug fix
    
    * Add transforms with targets
    
    * Fix typings
    
    * Add ToPILImage
    
    * update upload manager
    
    * fix extract classes
    
    * Allow jfif files to be used
    
    * Black and isort lint
    
    * Projects -> Datasets
    
    * Fix split issue
    
    * Refactor RandomHorizontalFlip and format
    
    * Use darwin.torch instead of darwin.pytorch
    
    * Validate image and video extensions
    
    * Validate image and video extensions
    
    * Update darwin/dataset/remote_dataset.py
    
    Co-Authored-By: Simon Edwardsson <simon.edwardsson@gmail.com>
    
    * Favour enumerate against range(len())
    
    * Simplify transforms
    
    * remove force_resplit
    
    * add support for local versions of the dataset
    
    * rename releases forlder
    
    * error message
    
    * remove latest symlink
    
    * format
    
    * implement latest
    
    * fix bounding box in get_annotations
    
    * fix again
    
    * elif
    
    * replace val with validation
    
    * improve efficiency when loading annotations
    
    * remove sklearn global dependency
    
    * yield in annotations
    
    * optional split in get_annotations to load all the annotations
    
    * add box in classes types
    
    * re-factor get_annotations
    
    * replace get_all_records with get_record
    
    * rename get_coco_format_record function
    
    * add option annotation_format
    
    * bumped version, added numpy as required
    
    * bug fix
    
    * bug fix
    
    * support for tif images
    
    * add warning
    
    * deprecated
    
    * Restores get_dataset() function in the darwin.torch toolbox (#73)
    
    * fix get_dataset
    * partition check
    * clearer import errors
    * Update README.md
    
    * autoflake
    
    * add team in local dataset structure
    
    * migration tool
    
    * fix darwin dataset path
    
    * remove unused exception
    
    * add missing check
    
    * Apply suggestions from code review
    
    Co-authored-by: Simon Edwardsson <simon.edwardsson@gmail.com>
    
    * add check for migrated datasets
    
    * update output
    
    * update old structure checks
    
    * add annotations check
    
    * fix box vs bounding_box
    
    * add split function in CLI
    
    Co-authored-by: Andrea Azzini <and.azzini@gmail.com>
    Co-authored-by: Simon Edwardsson <simon.edwardsson@gmail.com>

--- darwin/torch/__init__.py
+++ darwin/torch/__init__.py
@@ -1,7 +1,23 @@
-# Requirements: pytorch, torchvision, pycocotools, sklearn
+# Requirements: pytorch, torchvision, pycocotools
+try:
+    import torch  # noqa
+    import torchvision  # noqa
+except ImportError:
+    raise ImportError(
+        f"darwin.torch requires pytorch and torchvision. Install it using: pip install torch torchvision"
+    ) from None
+
+try:
+    import pycocotools  # noqa
+except ImportError:
+    raise ImportError(
+        f"darwin.torch requires pycocotools. Install it using: pip install cython; pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'"
+    ) from None
+
 from .dataset import (
-    ClassificationDataset,
+    get_dataset,
     Dataset,
+    ClassificationDataset,
     InstanceSegmentationDataset,
     SemanticSegmentationDataset,
 )
