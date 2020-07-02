c6ff2175a562460a6427f7c3eb78ce26210bb454
Jon Almazan <jon.almazan@gmail.com>
2020-07-02 14:56:16 +0100

    formatting

--- darwin/utils.py
+++ darwin/utils.py
@@ -226,9 +226,7 @@ def ispolygon(annotation):
     return annotation.annotation_type in ["polygon", "complex_polygon"]
 
 
-def convert_polygons_to_sequences(
-    polygons: List, height: Optional[int] = None, width: Optional[int] = None
-) -> List:
+def convert_polygons_to_sequences(polygons: List, height: Optional[int] = None, width: Optional[int] = None) -> List:
     """
     Converts a list of polygons, encoded as a list of dictionaries of into a list of nd.arrays
     of coordinates.
