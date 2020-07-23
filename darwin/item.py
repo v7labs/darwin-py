24d5e626b3f9122174131457afb6c0b0b85814fb
Simon Edwardsson <simon.edwardsson@gmail.com>
2020-07-23 11:03:39 +0100

    include current workflow id in dataset items

--- darwin/item.py
+++ darwin/item.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass(frozen=True, eq=True)
@@ -10,9 +11,17 @@ class DatasetItem:
     filesize: int
     dataset_id: int
     dataset_slug: str
+    current_workflow_id: Optional[int]
 
 
 def parse_dataset_item(raw) -> DatasetItem:
     return DatasetItem(
-        raw["id"], raw["filename"], raw["status"], raw["archived"], raw["file_size"], raw["dataset_id"], "n/a"
+        raw["id"],
+        raw["filename"],
+        raw["status"],
+        raw["archived"],
+        raw["file_size"],
+        raw["dataset_id"],
+        "n/a",
+        raw.get("current_workflow_id"),
     )
