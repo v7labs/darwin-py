794576e2febc7bf73d864d2c96d22e5843ee62e9
Jon Almazan <jon.almazan@gmail.com>
2020-09-04 14:43:20 +0100

    remove pycocotools dependency

--- darwin/torch/__init__.py
+++ darwin/torch/__init__.py
@@ -7,11 +7,4 @@ except ImportError:
         f"darwin.torch requires pytorch and torchvision. Install it using: pip install torch torchvision"
     ) from None
 
-try:
-    import pycocotools  # noqa
-except ImportError:
-    raise ImportError(
-        f"darwin.torch requires pycocotools. Install it using: pip install cython; pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'"
-    ) from None
-
 from .dataset import get_dataset  # noqa
