52a7e507fa2f8395d34d2db959b75abef61ee4fd
Jon Almazán <jon.almazan@gmail.com>
2020-11-05 20:47:56 +0000

    Parse Darwin Video (#114)
    Allows parsing and importing of Darwin JSON videos.

--- darwin/options.py
+++ darwin/options.py
@@ -75,11 +75,12 @@ class Options(object):
             help="Excludes the files with the specified extension/s if a data folder is provided as data path. ",
         )
         parser_push.add_argument(
-            "-f", "--fps", default="native", help="Frames per second for video split (recommended: 1), use 'native' to use the videos intrinsic fps."
+            "-f",
+            "--fps",
+            default="native",
+            help="Frames per second for video split (recommended: 1), use 'native' to use the videos intrinsic fps.",
         )
-        parser_push.add_argument(
-            "--frames", action="store_true", help="Annotate a video as independent frames"
-        )
+        parser_push.add_argument("--frames", action="store_true", help="Annotate a video as independent frames")
 
         parser_push.add_argument("--path", type=str, default=None, help="Folder to upload the files into.")
 
