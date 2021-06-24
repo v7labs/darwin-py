2ede0062d076a4d3cb281389f16e3b21612f3628
Andrea Azzini <and.azzini@gmail.com>
2021-06-24 18:28:16 +0200

    Change import in test

--- tests/darwin/item_test.py
+++ tests/darwin/item_test.py
@@ -1,8 +1,8 @@
-from darwin.item import DatasetItem
+import darwin
 
 
 def test_remote_path_gets_rid_of_initial_root_character_if_its_there():
-    dataset_item = DatasetItem(
+    dataset_item = darwin.item.DatasetItem(
         id=1,
         filename="filename.jpg",
         status="new",
@@ -18,7 +18,7 @@ def test_remote_path_gets_rid_of_initial_root_character_if_its_there():
 
 
 def test_remote_path_is_equal_to_path_otherwise():
-    dataset_item = DatasetItem(
+    dataset_item = darwin.item.DatasetItem(
         id=1,
         filename="filename.jpg",
         status="new",
@@ -34,7 +34,7 @@ def test_remote_path_is_equal_to_path_otherwise():
 
 
 def test_full_path_uses_remote_path():
-    dataset_item = DatasetItem(
+    dataset_item = darwin.item.DatasetItem(
         id=1,
         filename="filename.jpg",
         status="new",
