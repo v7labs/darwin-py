4eeaba6931618d66976592d2d016825a18817cb9
Brent Gracey <brentgracey@gmail.com>
2021-06-29 15:21:09 +0100

    Using PurePosixPath to avoid touching the OS and potentially getting different behaviour; for example; on Windows.

--- darwin/path_utils.py
+++ darwin/path_utils.py
@@ -1,8 +1,8 @@
-import os
+from pathlib import PurePosixPath
 from typing import Optional
 
 def construct_full_path(remote_path: Optional[str], filename: str) -> str:
     if remote_path is None:
         return filename
     else:
-        return os.path.join("/", remote_path, filename)
+        return (PurePosixPath("/") / remote_path / filename).as_posix()
--- tests/darwin/path_utils_test.py
+++ tests/darwin/path_utils_test.py
@@ -1,21 +1,23 @@
-import os
+from pathlib import PurePosixPath
 
 from darwin.path_utils import construct_full_path
 
+
 def test_os_path_construction():
-    # A quick reference for expected behavior of os.path.join
-    assert "/" == os.path.join("/", "/")
-    assert "/file.name" == os.path.join("/", "file.name")
-    assert "file.name" == os.path.join("", "file.name")
-    assert "/file.name" == os.path.join("", "/file.name")
-    assert "/file.name" == os.path.join("/", "/file.name")
+    # A quick reference for expected behavior of (PurePosixPath
+    assert "/test/foo.bar" == (PurePosixPath("/") / 'test' / 'foo.bar').as_posix()
+    assert "/" == (PurePosixPath("/") / "/").as_posix()
+    assert "/file.name" == (PurePosixPath("/") / "file.name").as_posix()
+    assert "file.name" == (PurePosixPath("") / "file.name").as_posix()
+    assert "/file.name" == (PurePosixPath("") / "/file.name").as_posix()
+    assert "/file.name" == (PurePosixPath("/") / "/file.name").as_posix()
     # note; this is not in /one path
-    assert "/file.name" == os.path.join("/one", "/file.name")
-    assert "/one/file.name" == os.path.join("/one", "file.name")
-    assert "/one/file.name" == os.path.join("/one/", "file.name")
-    assert "/one/two/file.name" == os.path.join("/", "one/two/", "file.name")
-    assert "/one/two/file.name" == os.path.join("/", "/one/two/", "file.name")
-    assert "/one/two/file.name" == os.path.join("/", "/one/two", "file.name")
-    assert "/one/two/three/file.name" == os.path.join("/", "one/", "two/", "three/" "file.name")
+    assert "/file.name" == (PurePosixPath("/one") / "/file.name").as_posix()
+    assert "/one/file.name" == (PurePosixPath("/one") / "file.name").as_posix()
+    assert "/one/file.name" == (PurePosixPath("/one/") / "file.name").as_posix()
+    assert "/one/two/file.name" == (PurePosixPath("/") / "one/two/" / "file.name").as_posix()
+    assert "/one/two/file.name" == (PurePosixPath("/") / "/one/two/" / "file.name").as_posix()
+    assert "/one/two/file.name" == (PurePosixPath("/") / "/one/two" / "file.name").as_posix()
+    assert "/one/two/three/file.name" == (PurePosixPath("/") / "one/" / "two/" / "three/" "file.name").as_posix()
     
     assert "onlyfile.name" == construct_full_path(None, "onlyfile.name")
