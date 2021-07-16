2794a48eafcb4e0f5a68a64f20f163fa7573e1a0
Pedro <palmelapedro@gmail.com>
2021-07-16 17:50:19 +0200

    removes test_prefix

--- tests/darwin/item_sorter_test.py
+++ tests/darwin/item_sorter_test.py
@@ -5,7 +5,7 @@ from darwin.item_sorter import ItemSorter, SortDirection
 
 def describe_item_sorter():
     def describe_parse():
-        def test_works_when_sort_is_complete_and_valid():
+        def works_when_sort_is_complete_and_valid():
             sort = "updated_at:asc"
 
             actual: ItemSorter = ItemSorter.parse(sort)
@@ -13,7 +13,7 @@ def describe_item_sorter():
             assert actual.field == "updated_at"
             assert actual.direction == SortDirection.ASCENDING
 
-        def test_works_when_sort_is_partial_and_valid():
+        def works_when_sort_is_partial_and_valid():
             sort = "updated_at"
 
             actual: ItemSorter = ItemSorter.parse(sort)
@@ -21,7 +21,7 @@ def describe_item_sorter():
             assert actual.field == "updated_at"
             assert actual.direction == SortDirection.ASCENDING
 
-        def test_raises_when_sort_has_invalid_format():
+        def raises_when_sort_has_invalid_format():
             sort = "updated_at:asc:desc:"
 
             with pytest.raises(ValueError) as error:
@@ -32,7 +32,7 @@ def describe_item_sorter():
                 in str(error.value)
             )
 
-        def test_raises_when_sort_has_invalid_field():
+        def raises_when_sort_has_invalid_field():
             field = "asdf"
             sort = f"{field}:asc"
 
@@ -47,21 +47,21 @@ def describe_item_sorter():
 
 def describe_sort_direction():
     def describe_parse():
-        def test_returns_asc_enum_when_given_asc_direction():
+        def returns_asc_enum_when_given_asc_direction():
             short_direction = "asc"
             long_direction = "ascending"
 
             assert SortDirection.parse(short_direction) == SortDirection.ASCENDING
             assert SortDirection.parse(long_direction) == SortDirection.ASCENDING
 
-        def test_returns_desc_enum_when_given_desc_direction():
+        def returns_desc_enum_when_given_desc_direction():
             short_direction = "desc"
             long_direction = "descending"
 
             assert SortDirection.parse(short_direction) == SortDirection.DESCENDING
             assert SortDirection.parse(long_direction) == SortDirection.DESCENDING
 
-        def test_raises_when_direction_is_invalid():
+        def raises_when_direction_is_invalid():
             direction = "bad_direction"
 
             with pytest.raises(ValueError) as error:
