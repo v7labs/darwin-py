6c09ae9298d0243289ec9b031292192b6cfe8e81
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-08-25 17:21:12 +0200

    Add default path to import annotation (#190)
    * identation fix
    
    * added docs adn typespecs
    
    * added tests for parse_darwin_json

--- tests/darwin/utils_test.py
+++ tests/darwin/utils_test.py
@@ -1,11 +1,13 @@
 from unittest.mock import MagicMock, patch
 
+from darwin.datatypes import AnnotationFile
 from darwin.utils import (
     is_extension_allowed,
     is_image_extension_allowed,
     is_project_dir,
     is_unix_like_os,
     is_video_extension_allowed,
+    parse_darwin_json,
     urljoin,
 )
 
@@ -74,3 +76,190 @@ def describe_is_unix_like_os():
     def it_returns_true_on_mac_os(mock: MagicMock):
         assert is_unix_like_os()
         mock.assert_called_once()
+
+
+def describe_parse_darwin_json():
+    def it_parses_darwin_images_correctly(tmp_path):
+        content = """
+        {
+            "image": {
+                "width": 497,
+                "height": 778,
+                "original_filename": "P49-RediPad-ProPlayLEFTY_442.jpg",
+                "filename": "P49-RediPad-ProPlayLEFTY_442.jpg",
+                "url": "",
+                "path": "/tmp_files"
+            },
+            "annotations": [
+                {
+                    "keypoint": {
+                        "x": 207.97048950195312,
+                        "y": 449.39691162109375
+                    },
+                    "name": "left_knee"
+                },
+                {
+                    "keypoint": {
+                        "x": 302.9606018066406,
+                        "y": 426.13946533203125
+                    },
+                    "name": "left_ankle"
+                }
+            ]
+        }
+        """
+
+        directory = tmp_path / "imports"
+        directory.mkdir()
+        import_file = directory / "darwin-file.json"
+        import_file.write_text(content)
+
+        annotation_file: AnnotationFile = parse_darwin_json(import_file, None)
+
+        assert annotation_file.path == import_file
+        assert annotation_file.filename == "P49-RediPad-ProPlayLEFTY_442.jpg"
+        assert len(annotation_file.annotations) == 2
+        assert len(annotation_file.annotation_classes) == 2
+        assert not annotation_file.is_video
+        assert annotation_file.image_width == 497
+        assert annotation_file.image_height == 778
+        assert annotation_file.image_url == ""
+        assert not annotation_file.workview_url
+        assert not annotation_file.seq
+        assert not annotation_file.frame_urls
+        assert annotation_file.remote_path == "/tmp_files"
+
+    def it_parses_darwin_videos_correctly(tmp_path):
+        content = """
+       {
+            "dataset": "my-dataset",
+            "image": {
+                "width": 3840,
+                "height": 2160,
+                "fps": 0.0,
+                "original_filename": "above tractor.mp4",
+                "filename": "above tractor.mp4",
+                "url": "https://my-website.com/api/videos/209/original",
+                "path": "/",
+                "workview_url": "https://my-website.com/workview?dataset=102&image=530",
+                "frame_count": 343,
+                "frame_urls": [
+                "https://my-website.com/api/videos/209/frames/0"
+                ]
+            },
+            "annotations": [
+                {
+                "frames": {
+                    "3": {
+                    "bounding_box": {
+                        "h": 547.0,
+                        "w": 400.0,
+                        "x": 363.0,
+                        "y": 701.0
+                    },
+                    "instance_id": {
+                        "value": 119
+                    },
+                    "keyframe": true,
+                    "polygon": {
+                        "path": [
+                        {
+                            "x": 748.0,
+                            "y": 732.0
+                        },
+                        {
+                            "x": 751.0,
+                            "y": 735.0
+                        },
+                        {
+                            "x": 748.0,
+                            "y": 733.0
+                        }
+                        ]
+                    }
+                    }
+                },
+                "interpolate_algorithm": "linear-1.1",
+                "interpolated": true,
+                "name": "Hand",
+                "segments": [
+                    [
+                    3,
+                    46
+                    ]
+                ]
+                }
+            ]
+            }
+        """
+
+        directory = tmp_path / "imports"
+        directory.mkdir()
+        import_file = directory / "darwin-file.json"
+        import_file.write_text(content)
+
+        annotation_file: AnnotationFile = parse_darwin_json(import_file, None)
+
+        assert annotation_file.path == import_file
+        assert annotation_file.filename == "above tractor.mp4"
+        assert len(annotation_file.annotations) == 1
+        assert len(annotation_file.annotation_classes) == 1
+        assert annotation_file.is_video
+        assert annotation_file.image_width == 3840
+        assert annotation_file.image_height == 2160
+        assert annotation_file.image_url == "https://my-website.com/api/videos/209/original"
+        assert annotation_file.workview_url == "https://my-website.com/workview?dataset=102&image=530"
+        assert not annotation_file.seq
+        assert annotation_file.frame_urls == ["https://my-website.com/api/videos/209/frames/0"]
+        assert annotation_file.remote_path == "/"
+
+    def it_returns_None_if_no_annotations_exist(tmp_path):
+        content = """
+        {
+            "image": {
+                "width": 497,
+                "height": 778,
+                "original_filename": "P49-RediPad-ProPlayLEFTY_442.jpg",
+                "filename": "P49-RediPad-ProPlayLEFTY_442.jpg",
+                "url": "",
+                "path": "/tmp_files"
+            }
+        }
+        """
+
+        directory = tmp_path / "imports"
+        directory.mkdir()
+        import_file = directory / "darwin-file.json"
+        import_file.write_text(content)
+
+        annotation_file: AnnotationFile = parse_darwin_json(import_file, None)
+
+        assert not annotation_file
+
+    def it_uses_a_default_path_if_one_is_missing(tmp_path):
+        content = """
+            {
+                "image": {
+                    "original_filename": "P49-RediPad-ProPlayLEFTY_442.jpg",
+                    "filename": "P49-RediPad-ProPlayLEFTY_442.jpg"
+                },
+                "annotations": [
+                    {
+                        "keypoint": {
+                            "x": 207.97048950195312,
+                            "y": 449.39691162109375
+                        },
+                        "name": "left_knee"
+                    }
+                ]
+            }
+            """
+
+        directory = tmp_path / "imports"
+        directory.mkdir()
+        import_file = directory / "darwin-file.json"
+        import_file.write_text(content)
+
+        annotation_file: AnnotationFile = parse_darwin_json(import_file, None)
+
+        assert annotation_file.remote_path == "/"
