30bdd51529028e7eff87929ec337cde503ae4908
Andrea Azzini <and.azzini@gmail.com>
2021-09-27 18:18:10 +0100

    Torch naming conflict hack

--- darwin/torch/__init__.py
+++ darwin/torch/__init__.py
@@ -1,7 +1,17 @@
-# Requirements: pytorch, torchvision
+import sys
+from pathlib import Path
+
 try:
-    import torch  # noqa
     import torchvision  # noqa
+
+    # Here we remove `darwin_directory` from `sys.path` to force the importer
+    # to import the library `torch`, rather than the internal package.
+    # This hack resolves this naming conflict for Sphinx.
+    darwin_directory = str(Path.home() / "darwin-py" / "darwin")
+    if darwin_directory in sys.path:
+        sys.path.remove(darwin_directory)
+
+    import torch  # noqa
 except ImportError:
     raise ImportError(
         f"darwin.torch requires pytorch and torchvision. Install it using: pip install torch torchvision"
