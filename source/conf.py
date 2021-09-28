11c23a8f7d7546bebf0aff154f12f8730e2bc683
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-09-28 18:16:27 +0200

    Sphinx Documentation (#211)

---
+++ source/conf.py
@@ -0,0 +1,67 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import re
+import sys
+from pathlib import Path
+from typing import List, Match, Optional
+
+sys.path.insert(0, os.path.abspath("../darwin/"))
+
+
+# -- Project information -----------------------------------------------------
+
+project: str = "darwin-py"
+copyright: str = "MIT License"
+author: str = "V7"
+
+# The full version, including alpha/beta/rc tags
+release: str = "0.0.0"
+
+with open(Path(__file__).parent.parent / "darwin" / "__init__.py", "r") as f:
+    # from https://www.py4u.net/discuss/139845
+    content: str = f.read()
+    search_result: Optional[Match[str]] = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content)
+    if search_result:
+        release = search_result.group(1)
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions: List[str] = [
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path: List[str] = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: List[str] = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme: str = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path: List[str] = []
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. darwin-py documentation master file, created by
+   sphinx-quickstart on Thu Aug  5 15:43:37 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+.. toctree::
+   :hidden:
+   :maxdepth: 3
+
+   Packages <modules>
+   README <README>
+
+.. include:: README.rst
