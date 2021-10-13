7bfa0e5b925fd378c0d7295e697ed4f79b1eb5ab
FrancescoSaverioZuppichini <francesco.zuppichini@gmail.com>
2021-10-13 11:34:32 +0100

    code refactor + comments

--- darwin/importer/importer.py
+++ darwin/importer/importer.py
@@ -1,5 +1,15 @@
 from pathlib import Path
-from typing import TYPE_CHECKING, Callable, Iterator, List, Optional, Set, Tuple, Union
+from typing import (
+    TYPE_CHECKING,
+    Callable,
+    Dict,
+    Iterator,
+    List,
+    Optional,
+    Set,
+    Tuple,
+    Union,
+)
 
 if TYPE_CHECKING:
     from darwin.client import Client
@@ -13,7 +23,7 @@ from darwin.utils import secure_continue_request
 from rich.progress import track
 
 
-def build_main_annotations_lookup_table(annotation_classes):
+def build_main_annotations_lookup_table(annotation_classes: List[Dict]) -> Dict[str, int]:
     MAIN_ANNOTATION_TYPES = [
         "bounding_box",
         "cuboid",
@@ -25,7 +35,7 @@ def build_main_annotations_lookup_table(annotation_classes):
         "skeleton",
         "tag",
     ]
-    lookup = {}
+    lookup: Dict[str, int] = {}
     for cls in annotation_classes:
         for annotation_type in cls["annotation_types"]:
             if annotation_type in MAIN_ANNOTATION_TYPES:
@@ -109,7 +119,7 @@ def import_annotations(
     file_paths: List[Union[str, Path]],
     append: bool,
     max_workers: int = 4,
-    force: bool = False,
+    require_user_confirm: bool = False,
 ) -> None:
     """
     Imports the given given Annotations into the given Dataset.
@@ -139,38 +149,50 @@ def import_annotations(
     """
     if not isinstance(file_paths, list):
         raise ValueError(f"file_paths must be a list of 'Path' or 'str'. Current value: {file_paths}")
-
+    # we need to fetch all the remote classes in order to find the correct id
     print("Fetching remote class list...")
-    team_classes = dataset.fetch_remote_classes(True)
-    classes_in_dataset = build_main_annotations_lookup_table([cls for cls in team_classes if cls["available"]])
-    classes_in_team = build_main_annotations_lookup_table([cls for cls in team_classes if not cls["available"]])
+    team_classes = dataset.fetch_remote_classes(team_wide=True)
+    # team-wide classes belonging to our dataset
+    classes_in_dataset = build_main_annotations_lookup_table(
+        [team_cls for team_cls in team_classes if team_cls["available"]]
+    )
+    # team-wide classes not yet belonging to any dataset
+    classes_in_team = build_main_annotations_lookup_table(
+        [team_cls for team_cls in team_classes if not team_cls["available"]]
+    )
+    # attributes are sub annotations and need a different look-up table
     attributes = build_attribute_lookup(dataset)
-
+    # we need to find and parse the specified files in `file_paths`
     print("Retrieving local annotations ...")
-    local_files = []
-    local_files_missing_remotely = []
-    parsed_files = list(find_and_parse(importer, file_paths))
-    filenames = [parsed_file.filename for parsed_file in parsed_files]
+    parsed_files: List[dt.AnnotationFile] = list(find_and_parse(importer, file_paths))
+    # filenames are the images' names
+    filenames: List[str] = [parsed_file.filename for parsed_file in parsed_files]
     print("Fetching remote file list...")
     # This call will only filter by filename; so can return a superset of matched files across different paths
     # There is logic in this function to then include paths to narrow down to the single correct matching file
-    remote_files = get_remote_files(dataset, filenames)
+    remote_path_to_item_id = get_remote_files(dataset, filenames)
+    local_files = []
+    local_files_missing_remotely = []
     for parsed_file in parsed_files:
-        if parsed_file.full_path not in remote_files:
+        if parsed_file.full_path not in remote_path_to_item_id:
             local_files_missing_remotely.append(parsed_file)
         else:
             local_files.append(parsed_file)
-
+    # now we have a list of missing files (images) that we have locally but not in the remote
+    # and a list of all the files we have both locally and in the remote
     print(f"{len(local_files) + len(local_files_missing_remotely)} annotation file(s) found.")
-    if local_files_missing_remotely:
+    missing_files_remotely: bool = len(local_files_missing_remotely) > 0
+    if missing_files_remotely:
         print(f"{len(local_files_missing_remotely)} file(s) are missing from the dataset")
         for local_file in local_files_missing_remotely:
             print(f"\t{local_file.path}: '{local_file.full_path}'")
 
-        if not force:
+        if not require_user_confirm:
+            # ask the user if he wants to import the annotations
             if not secure_continue_request():
-                return
-
+                # if user doesn't confirm, we exit the function
+                return None
+    # using the look-up defined before, we find out the classes that are not in the dataset and not in the team
     local_classes_not_in_dataset, local_classes_not_in_team = _resolve_annotation_classes(
         [annotation_class for file in local_files for annotation_class in file.annotation_classes],
         classes_in_dataset,
@@ -179,14 +201,14 @@ def import_annotations(
 
     print(f"{len(local_classes_not_in_team)} classes needs to be created.")
     print(f"{len(local_classes_not_in_dataset)} classes needs to be added to {dataset.identifier}")
-
+    # if we have skeletons, we need to have a specified class for each one of them
     missing_skeletons: List[dt.AnnotationClass] = list(filter(_is_skeleton_class, local_classes_not_in_team))
     missing_skeleton_names: str = ", ".join(map(_get_skeleton_name, missing_skeletons))
     if missing_skeletons:
         print(
             f"Found missing skeleton classes: {missing_skeleton_names}. Missing Skeleton classes cannot be created. Exiting now."
         )
-        return
+        return None
 
     if local_classes_not_in_team:
         print("About to create the following classes")
@@ -194,8 +216,9 @@ def import_annotations(
             print(
                 f"\t{missing_class.name}, type: {missing_class.annotation_internal_type or missing_class.annotation_type}"
             )
-        if not secure_continue_request():
-            return
+        if not require_user_confirm:
+            if not secure_continue_request():
+                return None
         for missing_class in local_classes_not_in_team:
             dataset.create_annotation_class(
                 missing_class.name, missing_class.annotation_internal_type or missing_class.annotation_type
@@ -212,21 +235,23 @@ def import_annotations(
         remote_classes = build_main_annotations_lookup_table(team_classes)
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        missing_files_paths: List[Path] = [missing_file.full_path for missing_file in local_files_missing_remotely]
 
-        def get_parsed_files(local_path: Path) -> List[Path]:
+        def get_uplodable_files(local_path: Path) -> List[dt.AnnotationFile]:
             parsed_files = importer(local_path)
             if type(parsed_files) is not list:
                 parsed_files = [parsed_files]
             # remove files missing on the server
-            missing_files = [missing_file.full_path for missing_file in local_files_missing_remotely]
-            parsed_files = [parsed_file for parsed_file in parsed_files if parsed_file.full_path not in missing_files]
-            return parsed_files
+            uploadable_files = [
+                parsed_file for parsed_file in parsed_files if parsed_file.full_path not in missing_files_paths
+            ]
+            return uploadable_files
 
-        def import_annotation(parsed_file: Path):
-            image_id = remote_files[parsed_file.full_path]
+        def import_annotation_from_annotation_file(parsed_file: dt.AnnotationFile):
+            item_id = remote_path_to_item_id[parsed_file.full_path]
             _import_annotations(
                 dataset.client,
-                image_id,
+                item_id,
                 remote_classes,
                 attributes,
                 parsed_file.annotations,
@@ -238,15 +263,13 @@ def import_annotations(
         # let's get the file paths we need
         paths: Set[Path] = set(local_file.path for local_file in local_files)
         # create our multi threading stage to get the files we need to import
-        parsed_files_stage: Iterator = executor.map(get_parsed_files, paths)
-        # resolve the stage
-        parsed_files: List[Path] = list(parsed_files_stage)
-        # out is a list of lists, we need to flat it
+        uploadable_files: List[List[dt.AnnotationFile]] = list(executor.map(get_uplodable_files, paths))
+        # parsed_files is a list of lists, we need to flat it
         # we also convert it to list because we need to know its len
-        parsed_files_flat: List[Path] = list(chain.from_iterable(parsed_files))
+        parsed_files_flat: List[dt.AnnotationFile] = list(chain.from_iterable(uploadable_files))
         # create our multi threading stage to import the annotations
         import_annotation_stage: Iterator = track(
-            executor.map(import_annotation, parsed_files_flat), total=len(parsed_files_flat)
+            executor.map(import_annotation_from_annotation_file, parsed_files_flat), total=len(parsed_files_flat)
         )
         # resolve the stage
         list(import_annotation_stage)
