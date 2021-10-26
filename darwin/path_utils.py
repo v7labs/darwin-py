7bc7ccf202420aaba124d25e55deaf9e9aefc452
Pedro <palmelapedro@gmail.com>
2021-10-26 15:28:17 +0200

    fixed type

--- darwin/path_utils.py
+++ darwin/path_utils.py
@@ -1,8 +1,10 @@
 from pathlib import PurePosixPath
 from typing import Optional, Tuple
 
+from datatypes import PathLike
 
-def construct_full_path(remote_path: Optional[str], filename: str) -> str:
+
+def construct_full_path(remote_path: Optional[str], filename: str) -> PathLike:
     if remote_path is None:
         return filename
     else:
