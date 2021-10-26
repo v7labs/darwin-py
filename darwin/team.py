35b604cb98d4463b3f0bdf0cc747720e129206a8
Pedro <palmelapedro@gmail.com>
2021-10-26 15:36:37 +0200

    added types to team

--- darwin/team.py
+++ darwin/team.py
@@ -1,6 +1,6 @@
 class Team:
-    def __init__(self, id, name, slug, selected=False):
-        self.id = id
-        self.name = name
-        self.slug = slug
-        self.selected = selected
+    def __init__(self, id: int, name: str, slug: str, selected: bool = False):
+        self.id: int = id
+        self.name: str = name
+        self.slug: str = slug
+        self.selected: bool = selected
