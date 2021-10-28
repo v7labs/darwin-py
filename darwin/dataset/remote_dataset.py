fdd208b9387f2e148eb0c407f1a831051d29f17b
Pedro <palmelapedro@gmail.com>
2021-10-28 13:33:02 +0200

    Added types to dataset/remote_dataset

--- darwin/dataset/remote_dataset.py
+++ darwin/dataset/remote_dataset.py
@@ -9,6 +9,7 @@ from typing import (
     Any,
     Callable,
     Dict,
+    Iterable,
     Iterator,
     List,
     Optional,
@@ -36,7 +37,7 @@ from darwin.dataset.utils import (
     make_class_lists,
     sanitize_filename,
 )
-from darwin.datatypes import AnnotationClass, AnnotationFile, PathLike, Team
+from darwin.datatypes import Annotation, AnnotationClass, AnnotationFile, PathLike, Team
 from darwin.exceptions import NotFound, UnsupportedExportFormat
 from darwin.exporter.formats.darwin import build_image_annotation
 from darwin.item import DatasetItem, parse_dataset_item
@@ -374,12 +375,12 @@ class RemoteDataset:
             else:
                 return
 
-    def archive(self, items) -> None:
+    def archive(self, items: Iterator[DatasetItem]) -> None:
         self.client.put(
             f"datasets/{self.dataset_id}/items/archive", {"filter": {"dataset_item_ids": [item.id for item in items]}}
         )
 
-    def restore_archived(self, items) -> None:
+    def restore_archived(self, items: Iterator[DatasetItem]) -> None:
         self.client.put(
             f"datasets/{self.dataset_id}/items/restore", {"filter": {"dataset_item_ids": [item.id for item in items]}}
         )
@@ -406,11 +407,14 @@ class RemoteDataset:
         """
         if not self.annotation_types:
             self.annotation_types = self.client.get("/annotation_types")
+
         for annotation_type in self.annotation_types:
             if annotation_type["name"] == name:
                 return annotation_type["id"]
 
-    def create_annotation_class(self, name: str, type: str, subtypes: List[str] = []) -> Dict:
+        return None
+
+    def create_annotation_class(self, name: str, type: str, subtypes: List[str] = []) -> Dict[str, Any]:
         """
         Creates an annotation class for this dataset.
 
@@ -459,7 +463,7 @@ class RemoteDataset:
             error_handlers=[name_taken, validation_error],
         )
 
-    def add_annotation_class(self, annotation_class: Union[AnnotationClass, int]) -> Union[Dict, None]:
+    def add_annotation_class(self, annotation_class: Union[AnnotationClass, int]) -> Optional[Dict[str, Any]]:
         """
         Adds an annotation class to this dataset.
 
@@ -533,11 +537,13 @@ class RemoteDataset:
                 classes_to_return.append(cls)
         return classes_to_return
 
-    def fetch_remote_attributes(self):
+    def fetch_remote_attributes(self) -> Any:
         """Fetches all remote attributes on the remote dataset"""
         return self.client.get(f"/datasets/{self.dataset_id}/attributes")
 
-    def export(self, name: str, annotation_class_ids: Optional[List[str]] = None, include_url_token: bool = False):
+    def export(
+        self, name: str, annotation_class_ids: Optional[List[str]] = None, include_url_token: bool = False
+    ) -> None:
         """
         Create a new release for the dataset
 
@@ -564,7 +570,7 @@ class RemoteDataset:
             error_handlers=[name_taken, validation_error],
         )
 
-    def get_report(self, granularity="day") -> str:
+    def get_report(self, granularity: str = "day") -> str:
         return self.client.get(
             f"/reports/{self.team}/annotation?group_by=dataset,user&dataset_ids={self.dataset_id}&granularity={granularity}&format=csv&include=dataset.name,user.first_name,user.last_name,user.email",
             team=self.team,
@@ -630,7 +636,7 @@ class RemoteDataset:
         split_seed: int = 0,
         make_default_split: bool = True,
         release_name: Optional[str] = None,
-    ):
+    ) -> None:
         """
         Creates lists of file names for each split for train, validation, and test.
         Note: This functions needs a local copy of the dataset
@@ -668,7 +674,7 @@ class RemoteDataset:
             make_default_split=make_default_split,
         )
 
-    def classes(self, annotation_type: str, release_name: Optional[str] = None):
+    def classes(self, annotation_type: str, release_name: Optional[str] = None) -> List[str]:
         """
         Returns the list of `class_type` classes
 
@@ -700,7 +706,7 @@ class RemoteDataset:
         annotation_type: str = "polygon",
         release_name: Optional[str] = None,
         annotation_format: Optional[str] = "darwin",
-    ):
+    ) -> Iterable[Dict[str, Any]]:
         """
         Returns all the annotations of a given split and partition in a single dictionary
 
@@ -740,7 +746,7 @@ class RemoteDataset:
         ):
             yield annotation
 
-    def workview_url_for_item(self, item):
+    def workview_url_for_item(self, item: DatasetItem) -> str:
         return urljoin(self.client.base_url, f"/workview?dataset={self.dataset_id}&image={item.seq}")
 
     @property
@@ -751,10 +757,14 @@ class RemoteDataset:
     @property
     def local_path(self) -> Path:
         """Returns a Path to the local dataset"""
-        if self.slug is not None:
-            return Path(self.client.get_datasets_dir(self.team)) / self.team / self.slug
+        datasets_dir: Optional[str] = self.client.get_datasets_dir(self.team)
+        if not datasets_dir:
+            raise ValueError(f"Unable to find datasets directory for {self.team}")
+
+        if self.slug:
+            return Path(datasets_dir) / self.team / self.slug
         else:
-            return Path(self.client.get_datasets_dir(self.team)) / self.team
+            return Path(datasets_dir) / self.team
 
     @property
     def local_releases_path(self) -> Path:
