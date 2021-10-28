7435d20046de1ca1343ee7e289d6c47ce9257cc4
Pedro <palmelapedro@gmail.com>
2021-10-28 12:43:12 +0200

    added more typings

--- darwin/validators.py
+++ darwin/validators.py
@@ -1,13 +1,15 @@
+from typing import Any, Dict
+
 from darwin.exceptions import NameTaken, ValidationError
 
 
-def name_taken(code, body) -> None:
+def name_taken(code: int, body: Dict[str, Any]) -> None:
     if code != 422:
         return
     if body.get("errors", {}).get("name") == ["has already been taken"]:
         raise NameTaken
 
 
-def validation_error(code, body) -> None:
+def validation_error(code: int, body: Dict[str, Any]) -> None:
     if code == 422:
         raise ValidationError(body)
