9e1e36b1cffbecd264cdb27500085ddc427db6ab
Pedro <palmelapedro@gmail.com>
2021-11-17 15:17:54 +0100

    added label box to list of supported formats

--- darwin/importer/formats/__init__.py
+++ darwin/importer/formats/__init__.py
@@ -2,9 +2,10 @@ from typing import List
 
 from darwin.datatypes import ImporterFormat
 
-from . import coco, csvtags, csvtagsvideo, darwin, dataloop, pascalvoc
+from . import coco, csvtags, csvtagsvideo, darwin, dataloop, label_box, pascalvoc
 
 supported_formats: List[ImporterFormat] = [
+    ("labelbox", label_box.parse_file),
     ("pascal_voc", pascalvoc.parse_file),
     ("dataloop", dataloop.parse_file),
     ("csv_tags", csvtags.parse_file),
