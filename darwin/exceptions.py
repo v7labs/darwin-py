fc9d991eba0c779cc8dd0bfe592b12db25cfc861
Pedro <palmelapedro@gmail.com>
2021-12-09 18:05:02 +0100

    Added docs

--- darwin/exceptions.py
+++ darwin/exceptions.py
@@ -1,27 +1,65 @@
+from pathlib import Path
+
+
 class Unauthenticated(Exception):
+    """
+    Used when a user tries to perform an action that requires authentication without being 
+    authenticated.
+    """
+
     pass
 
 
 class InvalidLogin(Exception):
+    """
+    Used when a user tries to log in with invalid credentials.
+    """
+
     pass
 
 
 class InvalidTeam(Exception):
+    """
+    Used when a team is not found or has no valid API key.
+    """
+
     pass
 
 
 class MissingConfig(Exception):
+    """
+    Used when the configuration file was not found.
+    """
+
     pass
 
 
 class UnsupportedExportFormat(Exception):
-    def __init__(self, format):
+    """
+    Used when one tries to export an annotation into a format that is not supported.
+    """
+
+    def __init__(self, format: str):
+        """
+        Parameters
+        ----------
+        format: str
+            The unsupported format.
+        """
         super().__init__()
         self.format = format
 
 
 class NotFound(Exception):
-    def __init__(self, name):
+    """Used when a given resource is not found."""
+
+    def __init__(self, name: str):
+        """
+        Parameters
+        ----------
+        name: str
+            The name of the resource.
+        """
         super().__init__()
         self.name = name
 
@@ -30,26 +68,56 @@ class NotFound(Exception):
 
 
 class UnsupportedFileType(Exception):
-    def __init__(self, path):
+    """
+    Used when a given does not have a supported video or image extension. 
+    """
+
+    def __init__(self, path: Path):
+        """
+        Parameters
+        ----------
+        path: Path
+            The path of the file.
+        """
         self.path = path
 
 
 class InsufficientStorage(Exception):
+    """
+    Used when a request to a server fails due to insufficient storage.
+    """
+
     pass
 
 
 class NameTaken(Exception):
+    """
+    Used when one tries to create an entity and the name of that entity is already taken.
+    """
+
     pass
 
 
 class ValidationError(Exception):
+    """
+    Used when a validation fails.
+    """
+
     pass
 
 
 class Unauthorized(Exception):
+    """
+    Used when a user tries to perform an action without having the necessary permissions.
+    """
+
     def __str__(self):
         return f"Unauthorized"
 
 
 class OutdatedDarwinJSONFormat(Exception):
+    """
+    Used when one tries to parse a video with an old darwin format that is no longer compatible.
+    """
+
     pass
