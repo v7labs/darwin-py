ed0abc7e370593374e06801a98f2685da6f37cfe
Pedro <palmelapedro@gmail.com>
2021-12-20 14:00:35 +0100

    fixing superannotate missing from import formats

--- darwin/importer/formats/__init__.py
+++ darwin/importer/formats/__init__.py
@@ -1,3 +1,12 @@
 from typing import List
 
-supported_formats: List[str] = ["coco", "dataloop", "csv_tags", "csv_tags_video", "darwin", "labelbox", "pascal_voc"]
+supported_formats: List[str] = [
+    "coco",
+    "dataloop",
+    "csv_tags",
+    "csv_tags_video",
+    "darwin",
+    "labelbox",
+    "pascal_voc",
+    "superannotate",
+]
