755c78a822d4a761ee57b6a7247e6d8776780858
Andrea Azzini <and.azzini@gmail.com>
2021-12-24 11:06:20 +0100

    rename test

--- tests/darwin/dataset/split_manager_test.py
+++ tests/darwin/dataset/split_manager_test.py
@@ -17,16 +17,11 @@ def test_requires_scikit_learn():
 
 def describe_classification_dataset():
     @pytest.mark.parametrize("val_percentage,test_percentage", [(0, 0.3), (0, 0), (0.2, 0), (0.5, 0.5), (1, 0.1)])
-    def it_should_split_a_dataset(
+    def it_raises_for_invalid_split_configuration(
         team_slug: str, team_extracted_dataset_path: Path, val_percentage: float, test_percentage: float
     ):
-        root = team_extracted_dataset_path / team_slug / "sl"
-
-        train_percentage: float = 1 - val_percentage - test_percentage
-
-        tot_size: int = len(list((root / "images").glob("*")))
-
         with pytest.raises(ValueError):
+            root = team_extracted_dataset_path / team_slug / "sl"
             split_dataset(root, release_name="latest", val_percentage=val_percentage, test_percentage=test_percentage)
 
     @pytest.mark.parametrize("val_percentage,test_percentage", [(0.2, 0.3), (0.3, 0.2)])
