31bd95eba901941ac49e755c5704e08f19ea75bb
Pedro <palmelapedro@gmail.com>
2022-01-21 16:20:21 +0000

    separated doc_enum into its own file

--- darwin/item_sorter.py
+++ darwin/item_sorter.py
@@ -1,20 +1,6 @@
-from enum import Enum
 from typing import Union
 
-
-class DocEnum(Enum):
-    """
-    Documenting Enums in Python is not supported by many tools. Therefore this class was created to
-    support just that. It is basically a hack to allow Enum documentation.
-    `See more here <https://stackoverflow.com/a/50473952/1337392>`
-    """
-
-    def __new__(cls, value, doc=None):
-        self = object.__new__(cls)  # calling super().__new__(value) here would fail
-        self._value_ = value
-        if doc is not None:
-            self.__doc__ = doc
-        return self
+from darwin.doc_enum import DocEnum
 
 
 class SortDirection(DocEnum):
