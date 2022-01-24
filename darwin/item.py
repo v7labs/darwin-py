b99e692cdebc12be2312d1c680b13ae7d73c2c2a
Pedro <palmelapedro@gmail.com>
2022-01-24 16:37:21 +0000

    added docs

--- darwin/item.py
+++ darwin/item.py
@@ -6,6 +6,44 @@ from darwin.path_utils import construct_full_path
 
 @dataclass(frozen=True, eq=True)
 class DatasetItem:
+    """
+    DatasetItems represent files that can be images or videos which belong to a dataset.
+    Said dataset can be initiated or not.
+
+    Attributes
+    ----------
+    id : int
+        The id of this ``DatasetItem``.
+    filename : str
+        The filename of this ``DatasetItem``.
+    status : str
+        The filename of this ``DatasetItem``. It can be: 
+        - "archived",
+        - "error",
+        - "uploading",
+        - "processing",
+        - "new",
+        - "annotate",
+        - "review",
+        - "complete"
+    archived : bool
+        Whether or not this item was soft deleted.
+    filesize : int
+        The size of this ``DatasetItem``'s file in bytes.
+    dataset_id : int
+        The id of the ``Dataset`` this ``DatasetItem`` belongs to.
+    dataset_slug : str
+        The slugified name of the ``Dataset`` this ``DatasetItem`` belongs to.
+    seq : int
+        The sequential value of this ``DatasetItem`` in relation to the ``Dataset`` it belongs to.
+        This allows us to know which items were added first and is used mostly for sorting purposes.
+    current_workflow_id : Optional[int], default : None
+        The id of this ``DatasetItem``'s workflow. A ``None`` value means this ``DatasetItem`` is 
+        new and was never worked on, or was reseted to the new state.
+    path : str
+        The darwin path to this ``DatasetItem``.
+    """
+
     id: int
     filename: str
     status: str
@@ -19,10 +57,26 @@ class DatasetItem:
 
     @property
     def full_path(self) -> str:
+        """
+        The full POSIX relative path of this ``DatasetItem``.
+        """
         return construct_full_path(self.path, self.filename)
 
 
 def parse_dataset_item(raw: Dict[str, Any]) -> DatasetItem:
+    """
+    Parses the given dictionary into a ``DatasetItem``. Raises if such is not possible.
+
+    Parameters
+    ----------
+    raw : Dict[str, Any]
+        The dictionary to parse.
+
+    Returns
+    -------
+    DatasetItem
+        A dataset item with the parsed information.
+    """
     return DatasetItem(
         raw["id"],
         raw["filename"],
