88a11b4379decbde06d3515931d4c2104bf5277b
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2022-02-14 09:52:24 +0100

    Update darwin/client.py
    Co-authored-by: Andrea Azzini <and.azzini@gmail.com>

--- darwin/client.py
+++ darwin/client.py
@@ -1080,7 +1080,7 @@ class Client:
                 raise InsufficientStorage()
 
     def _has_json_response(self, response: Response) -> bool:
-        return "application/json" in str(response.headers.get("content-type"))
+        return "application/json" in response.headers.get("content-type", "")
 
     def _get_response_debug_text(self, response: Response) -> str:
         if self._has_json_response(response):
