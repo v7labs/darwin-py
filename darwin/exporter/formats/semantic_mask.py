531ae9c8fe94f7b598224c613160644f037af84d
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2022-03-01 10:24:42 +0100

    added docs to darwin/exporter/formats/semantic_mask.py (#365)

--- darwin/exporter/formats/semantic_mask.py
+++ darwin/exporter/formats/semantic_mask.py
@@ -6,4 +6,14 @@ from darwin.exporter.formats.mask import export as export_mask
 
 
 def export(annotation_files: Iterable[dt.AnnotationFile], output_dir: Path) -> None:
+    """
+    Exports the given ``AnnotationFile``s into semantic masks inside of the given ``output_dir``.
+
+    Parameters
+    ----------
+    annotation_files : Iterable[dt.AnnotationFile]
+        The ``AnnotationFile``s to be exported.
+    output_dir : Path
+        The folder where the new semantic mask files will be.
+    """
     return export_mask(annotation_files=annotation_files, output_dir=output_dir, mode="rgb")
