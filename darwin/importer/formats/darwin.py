56b3740040c88f549c4b40e72bbccabe73f89139
Pedro <palmelapedro@gmail.com>
2022-03-21 16:45:27 +0100

    Added docs

--- darwin/importer/formats/darwin.py
+++ darwin/importer/formats/darwin.py
@@ -6,6 +6,20 @@ from darwin.utils import parse_darwin_json
 
 
 def parse_path(path: Path) -> Optional[dt.AnnotationFile]:
+    """
+    Parses the given file into a darwin ``AnnotationFile`` or returns ``None`` if the file does not
+    have a ``.json`` extension.
+
+    Parameters
+    ----------
+    path : Path
+        The ``Path`` of the file to parse.
+
+    Returns
+    -------
+    Optional[dt.AnnotationFile]
+        The ``AnnotationFile`` file or ``None`` if the file was not parseable.
+    """
     if path.suffix != ".json":
         return None
     return parse_darwin_json(path, 0)
