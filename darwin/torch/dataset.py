e9cc4d764843de6b6163d8fac4127942322bc999
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2022-03-23 13:27:04 +0100

    Update darwin/torch/dataset.py
    Co-authored-by: Andrea Azzini <and.azzini@gmail.com>

--- darwin/torch/dataset.py
+++ darwin/torch/dataset.py
@@ -36,7 +36,7 @@ def get_dataset(
     dataset_type : str
         The type of dataset ``["classification", "instance-segmentation", "object-detection", "semantic-segmentation"]``.
     partition : str, default: None
-        Selects one of the partitions ``[train, val, test, None]``.
+        Selects one of the partitions ``["train", "val", "test", None]``.
     split : str, default: "default"
         Selects the split that defines the percentages used.
     split_type : str, default: "random"
