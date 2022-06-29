4ec91d4e6ce4ba8c52d764c4dd8aad6034851dd4
Thomas Varsavsky <tom@v7labs.com>
2022-06-29 11:00:34 +0100

    fix for test location

--- tests/darwin/exporter/formats/export_nifti_test.py
+++ tests/darwin/exporter/formats/export_nifti_test.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import unittest
 from pathlib import Path
+from zipfile import ZipFile
 
 import darwin.datatypes as dt
 import nibabel as nib
@@ -14,7 +15,7 @@ from yaml import parse
 
 @pytest.fixture
 def video_annotation() -> dt.AnnotationFile:
-    return parse_darwin_json(Path("tests/v7/nifti/annotations/vol0.json"))
+    return parse_darwin_json(Path("v7/nifti/annotations/vol0.json"))
 
 
 def test_nifti(video_annotation: dt.AnnotationFile):
@@ -22,5 +23,5 @@ def test_nifti(video_annotation: dt.AnnotationFile):
         nifti.export([video_annotation], output_dir=tmpdir)
         image_id = os.path.splitext(video_annotation.filename)[0]
         export_im = nib.load(Path(tmpdir) / f"{image_id}.nii.gz").get_fdata()
-        expected_im = nib.load("tests/v7/nifti/annotations/output.nii").get_fdata()
+        expected_im = nib.load("v7/nifti/annotations/output.nii").get_fdata()
         assert np.allclose(export_im, expected_im)
