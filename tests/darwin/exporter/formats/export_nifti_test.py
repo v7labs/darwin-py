815e0bacf8e6a54e4f1eb70e081d3e119db1fb61
Thomas Varsavsky <tom@v7labs.com>
2022-09-14 15:52:12 +0100

    improved tests

--- tests/darwin/exporter/formats/export_nifti_test.py
+++ tests/darwin/exporter/formats/export_nifti_test.py
@@ -11,18 +11,21 @@ import pytest
 from darwin.exporter.exporter import darwin_to_dt_gen
 from darwin.exporter.formats import nifti
 from darwin.utils import parse_darwin_json
-from tests.fixtures import *
+
+# from tests.fixtures import *
 from yaml import parse
 
 
-def test_nifti(team_slug: str, team_extracted_dataset_path: Path):
+def test_nifti():
     with tempfile.TemporaryDirectory() as tmpdir:
-        with ZipFile("tests/v7 2 2.zip") as zfile:
+        with ZipFile("tests/data.zip") as zfile:
             zfile.extractall(tmpdir)
-        filepath = Path(tmpdir) / "v7 2 2/nifti/annotations"
+        filepath = Path(tmpdir) / "v7/nifti/releases/latest/annotations"
         filepaths = [f for f in filepath.iterdir() if f.suffix == ".json"]
         video_annotations = darwin_to_dt_gen(filepaths)
+        print([v.metadata for v in video_annotations])
         nifti.export(video_annotations, output_dir=tmpdir)
-        export_im = nib.load(Path(tmpdir) / "vol0_3_brain.nii.gz").get_fdata()
-        expected_im = nib.load("tests/output.nii.gz").get_fdata()
+        print(os.listdir(tmpdir))
+        export_im = nib.load(Path(tmpdir) / "vol0_brain.nii.gz").get_fdata()
+        expected_im = nib.load(filepath / "vol0_brain.nii.gz").get_fdata()
         assert np.allclose(export_im, expected_im)
