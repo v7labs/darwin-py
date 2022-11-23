6c7eaef795fd52f04e6230c1d533b3e6eaa1e9d5
Rafal Slota <rafal.s@v7labs.com>
2022-11-23 09:50:54 +0100

    fixup! [IO-492][external] Make tag_categories optional on coco

--- darwin/importer/formats/coco.py
+++ darwin/importer/formats/coco.py
@@ -68,7 +68,8 @@ def parse_json(path: Path, data: Dict[str, Any]) -> Iterator[dt.AnnotationFile]:
     annotations = data["annotations"]
     image_lookup_table = {image["id"]: image for image in data["images"]}
     category_lookup_table = {category["id"]: category for category in data["categories"]}
-    tag_category_lookup_table = {category["id"]: category for category in data.get("tag_categories", [])}
+    tag_categories = data.get("tag_categories") or []
+    tag_category_lookup_table = {category["id"]: category for category in tag_categories}
     image_annotations: Dict[str, Any] = {}
 
     for image in data["images"]:
