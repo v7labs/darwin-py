126c52fb3e274112ba9df607d443e83b82ebc629
Owen Jones <owen@v7labs.com>
2023-01-11 18:22:59 +0000

    WIP pyproject and poetry files

--- darwin/version/__init__.py
+++ darwin/version/__init__.py
@@ -1,1 +1,14 @@
-__version__ = "0.8.6"
+from os.path import dirname
+from pathlib import Path
+
+from toml import load
+
+version_path = Path(dirname(__file__)) / ".." / ".." / "pyproject.toml"
+
+pyproject = load(version_path.resolve())
+version = pyproject.get("tool", {}).get("poetry", {}).get("version")
+
+if version is None:
+    raise SystemExit("Project file doesn't contain a version")
+
+__version__ = version
