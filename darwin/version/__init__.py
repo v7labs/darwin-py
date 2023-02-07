2f2916c53d18a425fa4ca00b701a015886ab2d26
Owen Jones <owen@v7labs.com>
2023-02-07 14:12:48 +0000

    Version module now picks up version from file

--- darwin/version/__init__.py
+++ darwin/version/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.8.8"
+from re import MULTILINE, compile, search
+
+MATCH = compile(r"^version\s*=\s* \"(\d+\.\d+\.\d+)\"", flags=MULTILINE)
+
+with open("pyproject.toml", "r") as f:
+    content = f.read()
+    version_matches = search(MATCH, content)
+    if version_matches:
+        version = version_matches.group(1)
+    else:
+        raise ValueError("Could not find version in pyproject.toml")
+
+__version__ = version
