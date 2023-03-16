7de967665f3f23175c911b78d62fc96fa4358be9
Owen Jones <owen@v7labs.com>
2023-03-16 15:28:04 +0000

    Update to tests file

--- tests/darwin/exporter/formats/export_mask_test.py
+++ tests/darwin/exporter/formats/export_mask_test.py
@@ -1,32 +1,32 @@
 import pytest
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test_get_palette() -> None:
     ...
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test_validate_annotations() -> None:
     ...
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test__handle_mask() -> None:
     ...
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test__handle_raster_layer() -> None:
     ...
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test__handle_polygon() -> None:
     ...
 
 
-@pytest.skip("Not implemented")
+@pytest.mark.skip("Not implemented")
 def test_export() -> None:
     ...
 
