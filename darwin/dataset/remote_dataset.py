a116520165228e3574fa2e2b26349f3f28afd7d3
Brent Gracey <brentgracey@gmail.com>
2023-03-23 09:56:24 +0000

    [mod-720][Internal] Count the number of files that are actually downloaded. (#558)
    Terse and informative log additions

--- darwin/dataset/remote_dataset.py
+++ darwin/dataset/remote_dataset.py
@@ -235,6 +235,9 @@ class RemoteDataset(ABC):
         ValueError
             If darwin in unable to get ``Team`` configuration.
         """
+
+        console = self.console or Console()
+
         if release is None:
             release = self.get_release()
 
@@ -329,7 +332,12 @@ class RemoteDataset(ABC):
             if env_max_workers and int(env_max_workers) > 0:
                 max_workers = int(env_max_workers)
 
+            console.print(f"Going to download {str(count)} files to {self.local_images_path.as_posix()}.")
             exhaust_generator(progress=progress(), count=count, multi_threaded=multi_threaded, worker_count=max_workers)
+
+            downloaded_file_count = len([f for f in self.local_images_path.rglob('*') if f.is_file()])
+            console.print(f"Total file count after download completed {str(downloaded_file_count)}.")
+
             return None, count
         else:
             return progress, count
