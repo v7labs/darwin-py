c75f7f655c9a0171d801c04f32ac674a60678db2
Steve Anderson <steve@v7labs.com>
2023-04-03 09:57:30 +0100

    [MOD-656][internal] Remove another extraneous print call

--- darwin/cli.py
+++ darwin/cli.py
@@ -169,7 +169,6 @@ def _run(args: Namespace, parser: ArgumentParser) -> None:
             )
     elif args.command == "benchmarks":
         if args.action == "run":
-            print(args.files)
             f.begin_evaluation_run(args.target, args.name, args.files, args.format)
     # Annotation schema validation
     elif args.command == "validate":
