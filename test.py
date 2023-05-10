f2297852330f4e2a95c15621a52e075925b248c0
Nathan Perkins <nathan@v7labs.com>
2023-05-10 15:24:33 +0100

    fix for image extensions

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-from typing import Optional, Union
+from pathlib import Path
 
-from pydantic import BaseModel, HttpUrl, parse_obj_as, validator
+from darwin.dataset.local_dataset import LocalDataset
 
-
-class Config(BaseModel):
-    url: Union[HttpUrl, str]
-
-    @validator("url", always=True)
-    def validate_url(cls, v: Union[HttpUrl, str]) -> HttpUrl:
-        if isinstance(v, str):
-            return parse_obj_as(HttpUrl, v)
-        return v
-
-
-url = "http://test_url.com"
-test_config = Config(url=url)
-
-print(test_config.url)
-print(isinstance(test_config.url, HttpUrl))
-
-test_config2 = Config(url=parse_obj_as(HttpUrl, url))
-print(test_config2.url)
-print(isinstance(test_config2.url, HttpUrl))
-print(test_config2.url)
+path = Path("/Users/nathanperkins/.darwin/datasets/v7-labs/io-1072/")
+test = LocalDataset(path, "polygon")
