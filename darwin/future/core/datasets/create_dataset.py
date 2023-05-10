da2867f60938283f12a17364b75fc7558d10e370
Owen Jones <owen@v7labs.com>
2023-05-10 17:31:12 +0100

    Remove code from other tickets.

--- darwin/future/core/datasets/create_dataset.py
+++ darwin/future/core/datasets/create_dataset.py
@@ -6,14 +6,4 @@ from darwin.future.data_objects.dataset import Dataset
 
 
 def create_dataset(api_client: Client, name: str) -> Dataset:
-    """
-    Creates a new dataset for the given team
-    """
-    response = api_client.post(
-        "/datasets",
-        {
-            "name": name,
-        },
-    )
-
-    return parse_obj_as(Dataset, response)
+    ...
