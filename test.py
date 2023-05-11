9ebbca8cd2bb883abfcbb5727ef76834fb242775
Nathan Perkins <nathanjp91@gmail.com>
2023-05-11 10:06:47 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-from darwin.dataset.local_dataset import LocalDataset
-
-path = Path("/Users/nathanperkins/.darwin/datasets/v7-labs/io-1072/")
-test = LocalDataset(path, "polygon")
