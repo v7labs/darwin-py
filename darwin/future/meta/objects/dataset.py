b547c64b9f01a815bdf66b5c3ccde66dc8128da7
Owen Jones <owencjones+github@gmail.com>
2023-06-27 11:59:40 +0100

    [IO-1018][internal] Create Dataset meta method (#621)
    * Method in place, tests nearly complete
    
    * Tests in place and working
    
    ---------
    
    Co-authored-by: Owen Jones <owen@v7labs.com>

--- darwin/future/meta/objects/dataset.py
+++ darwin/future/meta/objects/dataset.py
@@ -1,5 +1,6 @@
 from typing import List, Optional, Tuple, Union
 
+from darwin.future.core.datasets.create_dataset import create_dataset
 from darwin.future.core.datasets.get_dataset import get_dataset
 from darwin.future.core.datasets.remove_dataset import remove_dataset
 from darwin.future.data_objects.dataset import Dataset
@@ -23,15 +24,50 @@ class DatasetMeta:
         # TODO: implement
         raise NotImplementedError()
 
-    def create_dataset(self) -> Dataset:
-        # TODO: implement in IO-1018
-        raise NotImplementedError()
+    def create_dataset(self, slug: str) -> Tuple[Optional[List[Exception]], Optional[Dataset]]:
+        """
+        Creates a new dataset for the given team
+
+        Parameters
+        ----------
+        slug: str [a-b0-9-_]
+            The slug of the dataset to create
+
+        Returns
+        -------
+        Tuple[Optional[List[Exception]], Optional[Dataset]]
+            A tuple containing a list of exceptions and the dataset created
+
+        """
+        exceptions = []
+        dataset: Optional[Dataset] = None
+
+        try:
+            self._validate_slug(slug)
+            dataset = create_dataset(self.client, slug)
+        except Exception as e:
+            exceptions.append(e)
+
+        return exceptions or None, dataset
 
     def update_dataset(self) -> Dataset:
         # TODO: implement in IO-1018
         raise NotImplementedError()
 
     def delete_dataset(self, dataset_id: Union[int, str]) -> Tuple[Optional[List[Exception]], int]:
+        """
+        Deletes a dataset by id or slug
+
+        Parameters
+        ----------
+        dataset_id: Union[int, str]
+            The id or slug of the dataset to delete
+
+        Returns
+        -------
+        Tuple[Optional[List[Exception]], int]
+            A tuple containing a list of exceptions and the number of datasets deleted
+        """
         exceptions = []
         dataset_deleted = -1
 
@@ -48,6 +84,22 @@ class DatasetMeta:
 
     @staticmethod
     def _delete_by_slug(client: MetaClient, slug: str) -> int:
+        """
+        (internal) Deletes a dataset by slug
+
+        Parameters
+        ----------
+        client: MetaClient
+            The client to use to make the request
+
+        slug: str
+            The slug of the dataset to delete
+
+        Returns
+        -------
+        int
+            The dataset deleted
+        """
         assert_is(isinstance(client, MetaClient), "client must be a MetaClient")
         assert_is(isinstance(slug, str), "slug must be a string")
 
@@ -61,8 +113,45 @@ class DatasetMeta:
 
     @staticmethod
     def _delete_by_id(client: MetaClient, dataset_id: int) -> int:
+        """
+        (internal) Deletes a dataset by id
+
+        Parameters
+        ----------
+        client: MetaClient
+            The client to use to make the request
+
+        dataset_id: int
+            The id of the dataset to delete
+
+        Returns
+        -------
+        int
+            The dataset deleted
+        """
         assert_is(isinstance(client, MetaClient), "client must be a MetaClient")
         assert_is(isinstance(dataset_id, int), "dataset_id must be an integer")
 
         dataset_deleted = remove_dataset(client, dataset_id)
         return dataset_deleted
+
+    @staticmethod
+    def _validate_slug(slug: str) -> None:
+        """
+        Validates a slug
+
+        Parameters
+        ----------
+        slug: str
+            The slug to validate
+
+        Raises
+        ------
+        AssertionError
+        """
+        slug_copy = str(slug).lower().strip()
+        assert_is(isinstance(slug_copy, str), "slug must be a string")
+        assert_is(len(slug_copy) > 0, "slug must not be empty")
+
+        VALID_SLUG_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789-_"
+        assert_is(all(c in VALID_SLUG_CHARS for c in slug_copy), "slug must only contain valid characters")
