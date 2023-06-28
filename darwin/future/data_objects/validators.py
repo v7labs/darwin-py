219c355e60b73fbcd7319055b51ec5508e8fa615
Owen Jones <owen@v7labs.com>
2023-06-28 12:44:03 +0100

    Validator with tests

--- darwin/future/data_objects/validators.py
+++ darwin/future/data_objects/validators.py
@@ -1,3 +1,9 @@
+from typing import Literal, Optional, Union
+from uuid import UUID
+
+from darwin.exceptions import DarwinException
+
+
 def parse_name(name: str) -> str:
     """
     A function to parse and validate a name
@@ -14,3 +20,30 @@ def parse_name(name: str) -> str:
     """
     assert isinstance(name, str)
     return name.lower().strip()
+
+
+def validate_uuid(uuid: Union[str, UUID]) -> bool:
+    """
+    Validates a uuid string
+
+    Parameters
+    ----------
+    uuid: str
+        - the uuid to validate
+
+    Returns
+    ----------
+    bool
+        - True if the uuid is valid, False otherwise
+    """
+    try:
+        if isinstance(uuid, UUID):
+            return True
+
+        UUID(uuid)
+        return True
+    except ValueError:
+        return False
+
+    except Exception as e:
+        raise DarwinException("Unexpected error validating uuid") from e
--- darwin/future/tests/data_objects/test_validators.py
+++ darwin/future/tests/data_objects/test_validators.py
@@ -1,9 +1,10 @@
-import unittest
 from typing import Union
+from unittest.mock import patch
 
 import pytest
 
-from darwin.future.data_objects.validators import parse_name
+from darwin.exceptions import DarwinException
+from darwin.future.data_objects.validators import parse_name, validate_uuid
 
 Simple = Union[list, tuple, dict, str, int, float]
 
@@ -30,3 +31,23 @@ def test_parse_name_parses_correctly(input: str, expected: str) -> None:
 def test_parse_name_raises_with_incorrect_input(input: Simple) -> None:
     with pytest.raises(AssertionError) as e_info:
         parse_name(input)  # type: ignore[arg-type]
+
+
+@pytest.mark.parametrize(
+    "input, expectation",
+    [
+        ("742d8ce8-15a7-11ee-aa88-7e0b62d29b23", True),
+        ("96bfb8de-ecb4-456d-a7a2-243408c77718", True),
+        ("c6437ef1-5b86-3a4e-a071-c2d4ad414e6x", False),
+        ("c6437ef1-5b86-3a4e-a071-c2d4ad414e6", False),
+    ],
+)
+def test_validate_uuid(input: str, expectation: bool) -> None:
+    assert validate_uuid(input) == expectation
+
+
+def test_validate_uuid_raises_if_unexpected_error_throw() -> None:
+    with pytest.raises(DarwinException):
+        with patch("darwin.future.data_objects.validators.UUID") as mock_uuid:
+            mock_uuid.side_effect = Exception("Unexpected error")
+            validate_uuid(1)  # type: ignore[arg-type]
