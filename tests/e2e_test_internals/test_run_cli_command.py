c93654ffe607377fbd29b80a379fd7caa5288bec
Owen Jones <owen@v7labs.com>
2023-07-13 10:07:12 +0100

    Test change

--- tests/e2e_test_internals/test_run_cli_command.py
+++ tests/e2e_test_internals/test_run_cli_command.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from unittest import mock
 
 import pytest
@@ -17,7 +18,7 @@ def test_does_not_allow_directory_traversal() -> None:
         assert excinfo.value == "Cannot pass directory traversal to 'run_cli_command'."
 
 
-@mock.patch("e2e_tests.run_cli_command.run")
+@mock.patch("e2e_tests.helpers.run")
 def test_passes_working_directory_to_run_cli_command(mock_subprocess_run: mock.Mock) -> None:
     mock_subprocess_run.reset_mock()
     run_cli_command("darwin --help", "/usr/bin")
@@ -27,7 +28,23 @@ def test_passes_working_directory_to_run_cli_command(mock_subprocess_run: mock.M
     assert mock_subprocess_run.call_args[1]["cwd"] == "/usr/bin"
 
 
-@mock.patch("e2e_tests.run_cli_command.run")
+@mock.patch("e2e_tests.helpers.run")
+def test_passes_back_returncode_stdout_and_stderr(mock_subprocess_run: mock.Mock) -> None:
+    CompletedProcess = namedtuple("CompletedProcess", ["returncode", "stdout", "stderr"])
+    mocked_output = CompletedProcess(returncode=137, stdout=b"stdout", stderr=b"stderr")
+
+    mock_subprocess_run.return_value = mocked_output
+
+    return_code, std_out, std_err = run_cli_command("darwin --help", "/usr/bin")
+
+    mock_subprocess_run.assert_called_once()
+
+    assert return_code == 137
+    assert std_out == "stdout"
+    assert std_err == "stderr"
+
+
+@mock.patch("e2e_tests.helpers.run")
 def test_does_not_pass_working_directory_to_run_cli_command(mock_subprocess_run: mock.Mock) -> None:
     mock_subprocess_run.reset_mock()
     run_cli_command("darwin --help")
