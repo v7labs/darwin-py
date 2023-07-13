689b13bbf29a77e3f77b6475a835bf041cd45c24
Owen Jones <owen@v7labs.com>
2023-07-13 09:50:12 +0100

    Restructure and working tests

--- tests/e2e_test_internals/test_run_cli_command.py
+++ tests/e2e_test_internals/test_run_cli_command.py
@@ -3,7 +3,7 @@ from unittest import mock
 import pytest
 
 from darwin.exceptions import DarwinException
-from e2e_tests import run_cli_command
+from e2e_tests.helpers import run_cli_command
 
 
 def test_does_not_allow_directory_traversal() -> None:
@@ -17,18 +17,24 @@ def test_does_not_allow_directory_traversal() -> None:
         assert excinfo.value == "Cannot pass directory traversal to 'run_cli_command'."
 
 
-@mock.patch("")
+@mock.patch("e2e_tests.run_cli_command.run")
 def test_passes_working_directory_to_run_cli_command(mock_subprocess_run: mock.Mock) -> None:
     mock_subprocess_run.reset_mock()
-    run_cli_command("darwin --help", working_directory="/usr/bin")
-    mock_subprocess_run.assert_called_with("darwin --help", working_directory="/usr/bin")
+    run_cli_command("darwin --help", "/usr/bin")
+
+    mock_subprocess_run.assert_called_once()
+    assert mock_subprocess_run.call_args[0][0] == "darwin --help"
+    assert mock_subprocess_run.call_args[1]["cwd"] == "/usr/bin"
 
 
-@mock.patch("")
+@mock.patch("e2e_tests.run_cli_command.run")
 def test_does_not_pass_working_directory_to_run_cli_command(mock_subprocess_run: mock.Mock) -> None:
     mock_subprocess_run.reset_mock()
     run_cli_command("darwin --help")
-    mock_subprocess_run.assert_called_with("darwin --help")
+
+    mock_subprocess_run.assert_called_once()
+    assert mock_subprocess_run.call_args[0][0] == "darwin --help"
+    assert "cwd" not in mock_subprocess_run.call_args[1]
 
 
 if __name__ == "__main__":
