c6f7e7af17128d77e5ad13476282e6436d1a2ac8
Owen Jones <owencjones+github@gmail.com>
2023-07-17 20:47:19 +0100

    [IO-1334][internal] E2E Testing config (#636)
    * WIP Adding Env setup
    
    * Simple first solution
    
    * Cleanup
    
    ---------
    
    Co-authored-by: Owen Jones <owen@v7labs.com>

---
+++ e2e_tests/exceptions.py
@@ -0,0 +1,19 @@
+"""Custom exceptions for the e2e_tests module."""
+
+from typing import Dict, List
+
+from pytest import PytestWarning
+
+
+class E2EException(PytestWarning):
+    """Base class for all exceptions in this module."""
+
+    ...
+
+
+class E2EEnvironmentVariableNotSet(E2EException):
+    """Raised when an environment variable is not set."""
+
+    def __init__(self, name: str, *args: List, **kwargs: Dict) -> None:
+        super().__init__(*args, **kwargs)
+        self.name = name
