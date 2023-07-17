b88ebe64a57aed11729900c3e1e5914bdc65dde2
Owen Jones <owen@v7labs.com>
2023-07-17 10:10:40 +0100

    Simple first solution

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -1,32 +1,33 @@
+from collections import namedtuple
 from os import environ
 from os.path import dirname, join
 from pathlib import Path
-from typing import Optional, Tuple
+from pprint import pprint
 
 import dotenv
 import pytest
 
+from e2e_tests.exceptions import E2EEnvironmentVariableNotSet
 
-def pytest_configure(_: pytest.Config) -> None:
-    ...
+
+def pytest_configure(config: pytest.Config) -> None:
+    config.addinivalue_line("addopts", "--ignore=../tests/, ../future")
 
 
 def pytest_sessionstart(session: pytest.Session) -> None:
-    dotenv_file_location: Path = Path(join(dirname(__file__), "../.env")).absolute()
-
-    if not dotenv_file_location.exists():
-        raise FileNotFoundError(f"Could not find .env file at {dotenv_file_location}")
-
-    dotenv.load_dotenv(dotenv_file_location)
+    # Use dotenv values _if_ present
+    dotenv_file_location: Path = Path(join(dirname(__file__), ".env")).resolve()
+    if dotenv_file_location.exists():
+        dotenv.load_dotenv(dotenv_file_location)
 
     server = environ.get("E2E_ENVIRONMENT")
     api_key = environ.get("E2E_API_KEY")
 
     if server is None:
-        raise ValueError("E2E_ENVIRONMENT is not set")
+        raise E2EEnvironmentVariableNotSet("E2E_ENVIRONMENT")
 
     if api_key is None:
-        raise ValueError("E2E_API_KEY is not set")
+        raise E2EEnvironmentVariableNotSet("E2E_API_KEY")
 
     if not isinstance(session.config.cache, pytest.Cache):
         raise TypeError("Pytest caching is not enabled, but E2E tests require it")
@@ -35,10 +36,16 @@ def pytest_sessionstart(session: pytest.Session) -> None:
     session.config.cache.set("api_key", api_key)
 
 
+ConfigValues = namedtuple("ConfigValues", ["server", "api_key"])
+
+
 @pytest.fixture(
     scope="session", autouse=True
 )  # autouse=True means that this fixture will be automatically used by all tests
-def config_values(session: pytest.Session) -> Tuple[str, str]:
+def config_values(request) -> ConfigValues:
+    pprint(request)
+    session = request.node.session
+
     if not isinstance(session.config.cache, pytest.Cache):
         raise TypeError("Pytest caching is not enabled, but E2E tests require it")
 
@@ -51,4 +58,4 @@ def config_values(session: pytest.Session) -> Tuple[str, str]:
     if api_key is None:
         raise ValueError("E2E_API_KEY is not set")
 
-    return server, api_key
+    return ConfigValues(server=server, api_key=api_key)
