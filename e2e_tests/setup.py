9ecf5261e8485e38bd7f28ad7981bc00bdb010ea
Owen Jones <owen@v7labs.com>
2023-07-18 10:20:58 +0100

    WIP: creating datasets

--- e2e_tests/setup.py
+++ e2e_tests/setup.py
@@ -1,11 +1,13 @@
 import random
 import string
 from dataclasses import dataclass
-from typing import List
+from typing import List, Literal
 
+import Faker
 import pytest
 import requests
 
+from e2e_tests.conftest import ConfigValues
 from e2e_tests.exceptions import E2EException
 
 
@@ -32,6 +34,14 @@ class E2ETestRunInfo:
     datasets: List[E2EDataset]
 
 
+def api_call(verb: Literal["get", "post", "put" "delete"], url: str, payload: dict, api_key: str) -> requests.Response:
+    headers = {"Authorization": f"Bearer {api_key}"}
+    action = getattr(requests, verb)
+
+    response = action(url, headers=headers, json=payload)
+    return response
+
+
 alphabet = string.ascii_lowercase + string.digits
 
 
@@ -52,9 +62,8 @@ def generate_prefix(length: int = 6) -> str:
     return "".join(random.choice(alphabet) for i in range(length))
 
 
-# TODO Create a a dataset for the tests to use
 def create_dataset(prefix: str) -> E2EDataset:
-    ...
+    name = f"{prefix}_{faker}_dataset"
 
 
 # TODO Create an item within the dataset for the tests to use
@@ -63,7 +72,7 @@ def create_item(prefix: str) -> E2EItem:
 
 
 # TODO: Main setup orchestration
-def setup() -> List[E2EDataset]:
+def setup(config: ConfigValues) -> List[E2EDataset]:
     number_of_datasets = 3
     number_of_items = 3
 
