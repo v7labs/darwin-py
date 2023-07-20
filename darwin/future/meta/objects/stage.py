27745584692cd93d4cc941375b09b7b4800ec840
Nathan Perkins <nathan@v7labs.com>
2023-07-20 17:41:32 +0100

    added stage syntax

--- darwin/future/meta/objects/stage.py
+++ darwin/future/meta/objects/stage.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 from typing import List, Optional
 from uuid import UUID
 
 from darwin.future.core.client import Client
 from darwin.future.core.items.get import get_item_ids_stage
+from darwin.future.core.items.move_items import move_items_to_stage
 from darwin.future.data_objects.workflow import WFStage
 from darwin.future.meta.objects.base import MetaBase
 
@@ -26,6 +29,18 @@ class StageMeta(MetaBase[WFStage]):
         return get_item_ids_stage(
             self.client, str(self.meta_params["team_slug"]), str(self.meta_params["dataset_id"]), self.id
         )
+
+    def move_attached_files_to_stage(self, new_stage_id: UUID) -> StageMeta:
+        assert self.meta_params["team_slug"] is not None and type(self.meta_params["team_slug"]) == str
+        assert self.meta_params["workflow_id"] is not None and type(self.meta_params["workflow_id"]) == UUID
+        assert self.meta_params["dataset_id"] is not None and type(self.meta_params["dataset_id"]) == int
+        slug, w_id, d_id = (
+            self.meta_params["team_slug"],
+            self.meta_params["workflow_id"],
+            self.meta_params["dataset_id"],
+        )
+        move_items_to_stage(self.client, slug, w_id, d_id, new_stage_id, self.item_ids)
+        return self
 
     @property
     def id(self) -> UUID:
--- darwin/future/meta/objects/workflow.py
+++ darwin/future/meta/objects/workflow.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from enum import auto
+from pathlib import Path
+from typing import List, Optional, Union
 from uuid import UUID
 
-from darwin.future.core.items.move_items import move_items_to_stage
-from darwin.future.data_objects.workflow import WFDataset, Workflow
+from darwin.cli_functions import upload_data
+from darwin.dataset.upload_manager import LocalFile
+from darwin.datatypes import PathLike
+from darwin.future.data_objects.workflow import WFDataset, WFType, Workflow
 from darwin.future.meta.objects.base import MetaBase
 from darwin.future.meta.queries.stage import StageQuery
 
@@ -38,15 +45,35 @@ class WorkflowMeta(MetaBase[Workflow]):
             raise ValueError("WorkflowMeta has no item")
         return self._item.name
 
-    def push_from_dataset_stage(self) -> None:
+    def push_from_dataset_stage(self) -> WorkflowMeta:
         assert self._item is not None
         assert self._item.dataset is not None
         stages = self.stages
-        assert len(stages) > 1
         ds_stage = stages[0]
-        item_ids = ds_stage.item_ids
-        assert ds_stage._item is not None
+        assert len(stages) > 1
+        assert ds_stage._item is not None and ds_stage._item.type == WFType.DATASET
         next_stage = ds_stage._item.edges[0].target_stage_id
         assert next_stage is not None
-        slug, d_id, w_id = self.meta_params["team_slug"], self._item.dataset.id, self._item.id
-        move_items_to_stage(self.client, str(slug), w_id, d_id, next_stage, item_ids)
+        ds_stage.move_attached_files_to_stage(next_stage)
+        return self
+
+    def upload_files(
+        self,
+        files: Optional[List[Union[PathLike, LocalFile]]],
+        files_to_exclude: Optional[List[PathLike]] = None,
+        fps: int = 1,
+        path: Optional[str] = None,
+        frames: bool = False,
+        extract_views: bool = False,
+        preserve_folders: bool = False,
+        verbose: bool = False,
+        auto_push: bool = True,
+    ) -> WorkflowMeta:
+        assert self._item is not None
+        assert self._item.dataset is not None
+        upload_data(
+            self.dataset.name, files, files_to_exclude, fps, path, frames, extract_views, preserve_folders, verbose
+        )
+        if auto_push:
+            self.push_from_dataset_stage()
+        return self
