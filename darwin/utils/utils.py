b70226f8c38eceaca3462a0428443490cb60f565
Alex Rozumii <brain-geek@users.noreply.github.com>
2023-08-10 13:45:32 +0100

    Fix field name in darwin slot parsing
    In schema https://darwin-public.s3.eu-west-1.amazonaws.com/darwin_json/2.1/schema.json
    we don't have frame_manifest field - we have frame_manifests instead.

--- darwin/utils/utils.py
+++ darwin/utils/utils.py
@@ -520,7 +520,7 @@ def _parse_darwin_slot(data: Dict[str, Any]) -> dt.Slot:
         fps=data.get("fps"),
         metadata=data.get("metadata"),
         segments=data.get("segments", []),
-        frame_manifest=data.get("frame_manifest"),
+        frame_manifest=data.get("frame_manifests"),
     )
 
 
