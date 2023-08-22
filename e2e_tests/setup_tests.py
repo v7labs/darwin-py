8bb61b8c04cb52d53ab41b14d625aeec86bec0ed
Owen <owen@owencjones.co.uk>
2023-08-22 13:59:01 +0100

    WIP on io-1609

--- e2e_tests/setup_tests.py
+++ e2e_tests/setup_tests.py
@@ -1,11 +1,12 @@
 import base64
 import random
 import string
+from curses import is_term_resized
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import List, Literal, Optional, Tuple
+from typing import List, Literal, Optional
 from uuid import UUID
 
 import numpy as np
@@ -23,14 +24,6 @@ from e2e_tests.exceptions import E2EException
 @dataclass
 class E2EAnnotation:
     annotation_data: JSONType
-
-
-@dataclass
-class E2EAnnotationClass:
-    name: str
-    slug: str
-    type: Literal["bbox", "polygon"]
-    id: int
 
 
 @dataclass
@@ -133,59 +126,6 @@ def generate_random_string(length: int = 6, alphabet: str = (string.ascii_lowerc
     return "".join(random.choice(alphabet) for i in range(length))
 
 
-def add_classes_to_team(
-    prefix: str, dataset: E2EDataset, config: ConfigValues
-) -> Tuple[E2EAnnotationClass, E2EAnnotationClass]:
-    """
-    Add classes to the team, one bbox and one polygon
-
-    Parameters
-    ----------
-    prefix : str
-        The prefix to use for the class names
-    config : ConfigValues
-        The config values to use
-
-    Returns
-    -------
-    Tuple[E2EAnnotationClass, E2EAnnotationClass]
-        The minimal info about the created classes
-    """
-    names = f"{prefix}_{generate_random_string(4)}_bbox_class", f"{prefix}_{generate_random_string(4)}_polygon_class"
-    types = E2EAnnotationType.bbox, E2EAnnotationType.polygon
-    name_types = zip(names, types)
-
-    host, api_key, team_slug = config.server, config.api_key, config.team_slug
-    url = f"{host}/api/teams/{team_slug}/annotation_classes"
-
-    if not url.startswith("http"):
-        raise E2EException(f"Invalid server URL {host} - need to specify protocol in var E2E_ENVIRONMENT")
-
-    output: List[E2EAnnotationClass] = []
-    try:
-        for name, type in name_types:
-            payload = class_create_payload_factory(name, type, dataset)
-            response = api_call("post", url, payload, api_key)
-
-            if not response.ok:
-                raise E2EException(f"Failed to create class {name} - {response.status_code} - {response.text}")
-
-            class_info = response.json()
-            output.append(
-                E2EAnnotationClass(
-                    name=class_info["name"],
-                    slug=str(class_info["name"]).lower().replace(" ", "_"),
-                    type="bbox" if type == E2EAnnotationType.bbox else "polygon",
-                    id=class_info["id"],
-                )
-            )
-        return output[0], output[1]
-
-    except Exception as e:
-        print(f"Failed to create classes {names} - {e}")
-        pytest.exit("Test run failed in test setup stage")
-
-
 def create_dataset(prefix: str, config: ConfigValues) -> E2EDataset:
     """
     Create a randomised new dataset, and return its minimal info for reference
@@ -226,6 +166,78 @@ def create_dataset(prefix: str, config: ConfigValues) -> E2EDataset:
     except Exception as e:
         print(f"Failed to create dataset {name} - {e}")
         pytest.exit("Test run failed in test setup stage")
+
+
+def create_annotation(item: JSONType, config: ConfigValues) -> E2EAnnotation:
+    if "id" not in item:
+        raise E2EException(f"Invalid item {item} - no id")
+    else:
+        item_id = getattr(item, "id", None)
+
+    team_slug = config.team_slug
+    host, api_key = config.server, config.api_key
+    url = f"{host}/api/v2/teams/{team_slug}/items/{is_term_resized}/import"
+
+    x = random.randint(0, 100)
+    y = random.randint(0, 100)
+    w = random.randint(0, 100)
+    h = random.randint(0, 100)
+
+    class_id = generate_random_string(4)
+
+    if w + x > 100:
+        w = 100 - x
+
+    if h + y > 100:
+        h = 100 - y
+
+    try:
+        payload = {
+            # TODO: THIS IS THE PART CURRENTLY NOT WORKING
+            "annotations": [
+                {
+                    "annotation_class_id": class_id,
+                    "annotation_group_id": str(UUID4()),
+                    "data": {
+                        # fmt: off
+                        "bounding_box": {
+                            "h": h,
+                            "w": w,
+                            "x": x,
+                            "y": y,
+                        },
+                        "id": str(UUID4()),
+                        "name": "bbox_text",
+                        "slot_names": [
+                            "0"
+                        ]
+                        # fmt: on
+                    },
+                    "id": str(UUID4()),
+                }
+            ],
+        }
+
+        response = api_call("post", url, payload, api_key)
+
+        if response.ok:
+            annotation_info = response.json()
+            return E2EAnnotation(annotation_data=annotation_info["annotations"][0]["data"])
+        else:
+            raise E2EException(f"Failed to annotation for item {item.name} - {response.status_code} - {response.text}")
+    except E2EException as e:
+        print(f"Failed to annotation for item {item.name} - {e}")
+        pytest.exit("Test run failed in test setup stage")
+
+
+def add_annotations_to_item(item_info: JSONType, config: ConfigValues) -> List[E2EAnnotation]:
+    annotations: List[E2EAnnotation] = []
+
+    for i in range(3):
+        annotation = create_annotation(item_info, config)
+        annotations.append(annotation)
+
+    return annotations
 
 
 def create_item(dataset_slug: str, prefix: str, image: Path, config: ConfigValues) -> E2EItem:
@@ -284,6 +296,8 @@ def create_item(dataset_slug: str, prefix: str, image: Path, config: ConfigValue
 
             item_info = response.json()["items"][0]
 
+            annotations: List[E2EAnnotation] = add_annotations_to_item(item_info, config)
+
             return E2EItem(
                 name=item_info["name"],
                 id=item_info["id"],
@@ -301,65 +315,6 @@ def create_item(dataset_slug: str, prefix: str, image: Path, config: ConfigValue
 
     except Exception as e:
         print(f"Failed to create item {name} - {e}")
-        pytest.exit("Test run failed in test setup stage")
-
-
-def create_annotation(prefix: str, item: E2EItem, class_id: int, config: ConfigValues) -> E2EAnnotation:
-    team_slug = config.team_slug
-    host, api_key = config.server, config.api_key
-    url = f"{host}/api/v2/teams/{team_slug}/items/{item.id}/import"
-
-    x = random.randint(0, 100)
-    y = random.randint(0, 100)
-    w = random.randint(0, 100)
-    h = random.randint(0, 100)
-
-    if w + x > 100:
-        w = 100 - x
-
-    if h + y > 100:
-        h = 100 - y
-
-    try:
-        payload = {
-            # TODO: THIS IS THE PART CURRENTLY NOT WORKING
-            "annotations": [
-                {
-                    "annotation_class_id": class_id,
-                    "annotation_group_id": str(UUID4()),
-                    "data": {
-                        # fmt: off
-                        "bounding_box": {
-                            "h": h,
-                            "w": w,
-                            "x": x,
-                            "y": y,
-                        },
-                        "id": str(UUID4()),
-                        "name": "bbox_text",
-                        "slot_names": [
-                            "0"
-                        ]
-                        # fmt: on
-                    },
-                    "id": str(UUID4()),
-                }
-            ],
-        }
-
-        response = api_call("post", url, payload, api_key)
-
-        if response.ok:
-            annotation_info = response.json()
-            return E2EAnnotation(annotation_data=annotation_info["annotations"][0]["data"])
-
-        raise E2EException(f"Failed to annotation for item {item.name} - {response.status_code} - {response.text}")
-    except E2EException as e:
-        print(f"Failed to annotation for item {item.name} - {e}")
-        pytest.exit("Test run failed in test setup stage")
-
-    except Exception as e:
-        print(f"Failed to create item {item.name} - {e}")
         pytest.exit("Test run failed in test setup stage")
 
 
@@ -387,7 +342,7 @@ def create_random_image(prefix: str, directory: Path, height: int = 100, width: 
     return directory / image_name
 
 
-def setup_tests(config: ConfigValues) -> Tuple[List[E2EDataset], List[E2EAnnotationClass]]:
+def setup_tests(config: ConfigValues) -> List[E2EDataset]:
     """
     Setup data for End to end test runs
 
@@ -407,14 +362,12 @@ def setup_tests(config: ConfigValues) -> Tuple[List[E2EDataset], List[E2EAnnotat
         number_of_annotations = 3
 
         datasets: List[E2EDataset] = []
-        classes: List[E2EAnnotationClass] = []
 
         try:
             prefix = generate_random_string()
 
             for _ in range(number_of_datasets):
                 dataset = create_dataset(prefix, config)
-                classes += list(add_classes_to_team(prefix, dataset, config))
 
                 for _ in range(number_of_items):
                     image_for_item = create_random_image(prefix, Path(temp_directory))
@@ -435,12 +388,10 @@ def setup_tests(config: ConfigValues) -> Tuple[List[E2EDataset], List[E2EAnnotat
             print(e)
             pytest.exit("Setup failed - unknown error")
 
-        return datasets, classes
+        return datasets
 
 
-def teardown_tests(
-    config: ConfigValues, datasets: List[E2EDataset], classes: Tuple[E2EAnnotationClass, E2EAnnotationClass]
-) -> None:
+def teardown_tests(config: ConfigValues, datasets: List[E2EDataset]) -> None:
     """
     Teardown data for End to end test runs
 
