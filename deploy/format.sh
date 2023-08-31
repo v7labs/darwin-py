cce8fffef97174a20b43558176096b7537a30f66
Owen <owen@owencjones.co.uk>
2023-08-31 10:00:13 +0100

    Refactor of bash scripts

--- deploy/format.sh
+++ deploy/format.sh
@@ -16,7 +16,6 @@ if ! $CI; then
     echo "This script is intended for CI/CD only"
     exit 1
 fi
-
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <file>...<file>..."
     echo
@@ -37,55 +36,63 @@ echo
 # Check dependencies
 "$THIS_FILE_DIRECTORY"/check_python.sh || "$THIS_FILE_DIRECTORY"/install_deps.sh || exit 2
 
-EXIT_CODE=0
-NUMBER_OF_PYTHON_FILES=0
-SKIPPED_FILES=0
-NONEXISTENT_FILES=0
+
+black_failed_files=""
+number_of_python_files=0
+skipped_files=0
+skipped_init_files=0
+nonexistent_files=0
+
 for file in $FILES_CHANGED; do
     if [ ! -f "$file" ]; then
-        SKIPPED_FILES=$((SKIPPED_FILES+1))
-        NONEXISTENT_FILES=$((NONEXISTENT_FILES+1))
+        skipped_files=$((skipped_files+1))
+        nonexistent_files=$((nonexistent_files+1))
         continue
     fi
     
     if [[ $file == *"__init__.py"* ]]; then
-        SKIPPED_FILES=$((SKIPPED_FILES+1))
+        skipped_files=$((skipped_files+1))
         continue
     fi
     
     if [[ $file != *.py ]]; then
-        SKIPPED_FILES=$((SKIPPED_FILES+1))
+        skipped_files=$((skipped_files+1))
         continue
     fi
     
     echo "> Checking black formatting of file: $file"
+    number_of_python_files=$((number_of_python_files+1))
+    
     poetry run black --check --diff --no-color $file
-    NUMBER_OF_PYTHON_FILES=$((NUMBER_OF_PYTHON_FILES+1))
     if $!; then
         echo ">...Black formatting failed for file: $file"
-        EXIT_CODE=$!
+        black_failed_files="$black_failed_files $file"
     fi
     echo
 done
 
 echo "Done."
-echo "Checked $NUMBER_OF_PYTHON_FILES python files"
-echo "Skipped $SKIPPED_FILES files"
-echo "Skipped $NONEXISTENT_FILES files that do not exist"
+echo "Checked $number_of_python_files python files"
+echo "Skipped $skipped_files files"
+echo "Skipped $nonexistent_files files that do not exist"
 
-if [[ $NUMBER_OF_PYTHON_FILES -eq 0 ]]; then
+if [[ $number_of_python_files -eq 0 ]]; then
     echo "No checkable python files found in input."
     exit 0
 fi
 
-if [[ $EXIT_CODE -eq 0 ]]; then
-    if [[ $NUMBER_OF_PYTHON_FILES -eq 0 ]]; then
+if [[ $black_failed_files -eq "" ]]; then
+    if [[ $number_of_python_files -eq 0 ]]; then
         echo "No checkable python files found in input."
     else
         echo "Black formatting passed"
     fi
     exit 0
 else
-    echo "Black formatting failed"
+    echo "** Black formatting failed **"
+    echo "These files failed black formatting:"
+    echo
+    echo $black_failed_files | tr " " "\n"
+    echo
     exit 3
 fi
--- deploy/lint.sh
+++ deploy/lint.sh
@@ -11,46 +11,78 @@
 THIS_FILE_DIRECTORY=`dirname "$0"`
 FILES_CHANGED="$@"
 
+# Input checks
 if ! $CI; then
     echo "This script is intended for CI/CD only"
     exit 1
 fi
-
-echo "Checking linting compliance"
-
-
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <files-list>"
+    echo "Usage: $0 <file>...<file>..."
+    echo
+    echo "Called with $@"
     exit 1
 fi
 
-EXIT_CODE=0
+# Introduction
+echo
+echo "Checking linting compliance"
+echo
+echo "These files were changed in this diff:"
+echo $FILES_CHANGED | tr " " "\n"
+echo
+echo "** Checking formatting of files **"
+echo
 
-echo "Checking linting compliance of files: $FILES_CHANGED"
 "$THIS_FILE_DIRECTORY"/check_python.sh || "$THIS_FILE_DIRECTORY"/install_deps.sh || exit 2
 
+flake8_failed_files=""
+number_of_python_files=0
+skipped_files=0
+skipped_init_files=0
+nonexistent_files=0
+
 for file in $FILES_CHANGED; do
+    if [ ! -f "$file" ]; then
+        skipped_files=$((skipped_files+1))
+        nonexistent_files=$((nonexistent_files+1))
+        continue
+    fi
     if [[ $file == *"__init__.py"* ]]; then
-        echo "Skipping __init__.py file: $file"
+        skipped_files=$((skipped_files+1))
         continue
     fi
     if [[ $file != *.py ]]; then
-        echo "Skipping non-python file: $file"
+        skipped_files=$((skipped_files+1))
         continue
     fi
-
-    echo "Checking flake8 compliance of file: $file"
+    
+    echo "> Checking flake8 compliance of file: $file"
+    number_of_python_files=$((number_of_python_files+1))
+    
     poetry run flake8 $file
     if $!; then
         echo "Flake8 check failed for file: $file"
-        EXIT_CODE=$1!
+        flake8_failed_files="$flake8_failed_files $file"
     fi
 done
 
-if [[ $EXIT_CODE -eq 0 ]]; then
-    echo "Flake8 check passed"
-else
-    echo "Flake8 check failed"
+if [[ $number_of_python_files -eq 0 ]]; then
+    echo "No checkable python files found in input."
+    exit 0
 fi
 
-exit $EXIT_CODE
+if [[ $flake8_failed_files -eq "" ]]; then
+    if [[ $number_of_python_files -eq 0 ]]; then
+        echo "No checkable python files found in input."
+    else
+        echo "Black formatting passed"
+    fi
+    exit 0
+else
+    echo "** Black formatting failed **"
+    echo "These files failed black formatting:"
+    echo
+    echo $flake8_failed_files | tr " " "\n"
+    echo
+    exit 3
+fi
