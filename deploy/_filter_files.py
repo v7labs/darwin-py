ae1e8e3bf0d1ed6b5e068fedc66397151f21616e
Owen <owen@owencjones.co.uk>
2023-09-01 14:50:54 +0100

    File filter in python

---
+++ deploy/_filter_files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+from typing import List
+
+
+def main(argv: List[str]) -> None:
+    file_extension: str = argv[0]
+    files_in: List[str] = argv[1:]
+
+    if file_extension.startswith("."):
+        file_extension = file_extension[1:]
+
+    files_out = [file for file in files_in if file.endswith(f".{file_extension}")]
+
+    sys.stdout.write(" ".join(files_out))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
