b56eb84180fcb517b7f3d7cc86fd253946d9e363
Nathan Perkins <nathanjp91@gmail.com>
2023-09-18 10:23:05 +0100

    potential windows fix

--- e2e_tests/test_darwin.py
+++ e2e_tests/test_darwin.py
@@ -18,9 +18,7 @@ def new_dataset() -> E2EDataset:
     new_dataset_name = "test_dataset_" + uuid_str
     result = run_cli_command(f"darwin dataset create {new_dataset_name}")
     assert result[0] == 0
-    id_raw = re.findall(r"datasets[/|\\+](\d+)", result[1])
-    print(result)
-    print(id_raw)
+    id_raw = re.findall(r"datasets[/\\+](\d+)", result[1])
     assert id_raw is not None and len(id_raw) == 1
     id = int(id_raw[0])
     teardown_dataset = E2EDataset(id, new_dataset_name, None)
