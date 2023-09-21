c878d4fe773f9d3cea841d512b783128ce1ae6b6
Nathan Perkins <nathanjp91@gmail.com>
2023-09-21 16:36:06 +0100

    export tests and remote cleanup

--- e2e_tests/test_darwin.py
+++ e2e_tests/test_darwin.py
@@ -1,14 +1,17 @@
+import json
+import os
 import re
 import tempfile
 import uuid
 from pathlib import Path
+from time import sleep
 from typing import Generator
 
 import pytest
 
 from e2e_tests.helpers import run_cli_command
-from e2e_tests.objects import E2EDataset
-from e2e_tests.setup_tests import create_random_image
+from e2e_tests.objects import ConfigValues, E2EDataset, E2EItem
+from e2e_tests.setup_tests import api_call, create_random_image
 
 
 @pytest.fixture
@@ -25,6 +28,7 @@ def new_dataset() -> E2EDataset:
 
     # Add the teardown dataset to the pytest object to ensure it gets deleted when pytest is done
     pytest.datasets.append(teardown_dataset)  # type: ignore
+    sleep(2) # wait for dataset to be created on server
     return teardown_dataset
 
 
@@ -38,9 +42,38 @@ def local_dataset(new_dataset: E2EDataset) -> Generator[E2EDataset, None, None]:
 @pytest.fixture
 def local_dataset_with_images(local_dataset: E2EDataset) -> E2EDataset:
     assert local_dataset.directory is not None
-    [create_random_image(local_dataset.slug, Path(local_dataset.directory)) for x in range(3)]
+    for x in range(3):
+        path = create_random_image(str(x), Path(local_dataset.directory), fixed_name=True)
+        local_dataset.add_item(
+            E2EItem(
+                name=path.name,
+                id=uuid.uuid4(), # random uuid as only need item for annotation later
+                path=str(path),
+                file_name=path.name,
+                slot_name="",
+                annotations=[]
+            )
+        )
     return local_dataset
 
+
+def basic_annotation(name: str) -> dict:
+    with open("e2e_tests/data/base_annotation.json", "r") as f:
+        annotation = json.load(f)
+    annotation["item"]["name"] = name
+    return annotation
+
+@pytest.fixture
+def local_dataset_with_annotations(local_dataset_with_images: E2EDataset) -> E2EDataset:
+    assert local_dataset_with_images.directory is not None
+    dir = Path(local_dataset_with_images.directory) / "annotations"
+    os.mkdir(dir)
+    for item in local_dataset_with_images.items:
+        annotation = basic_annotation(item.name)
+        filename = dir / f"{item.name[:-4]}.json"
+        with open(filename, "w") as f:
+            json.dump(annotation, f)
+    return local_dataset_with_images
 
 def test_darwin_create(local_dataset: E2EDataset) -> None:
     """
@@ -49,7 +82,6 @@ def test_darwin_create(local_dataset: E2EDataset) -> None:
     """
     assert local_dataset.id is not None
     assert local_dataset.name is not None
-
 
 def test_darwin_push(local_dataset_with_images: E2EDataset) -> None:
     """
@@ -63,6 +95,55 @@ def test_darwin_push(local_dataset_with_images: E2EDataset) -> None:
     )
     assert result[0] == 0
 
+def test_darwin_import(local_dataset_with_annotations: E2EDataset) -> None:
+    """
+    Test importing a dataset via the darwin cli, dataset created via fixture with annotations added to objects
+    """
+    assert local_dataset_with_annotations.id is not None
+    assert local_dataset_with_annotations.name is not None
+    assert local_dataset_with_annotations.directory is not None
+    result = run_cli_command(
+        f"darwin dataset push {local_dataset_with_annotations.name} {local_dataset_with_annotations.directory}"
+    )
+    assert result[0] == 0
+    sleep(2)
+    result = run_cli_command(
+        f"yes Y | darwin dataset import {local_dataset_with_annotations.name} darwin {Path(local_dataset_with_annotations.directory) / 'annotations'}"
+    )
+    assert result[0] == 0
+
+
+def test_darwin_export(local_dataset_with_annotations: E2EDataset, config_values: ConfigValues) -> None:
+    """
+    Test exporting a dataset via the darwin cli, dataset created via fixture with annotations added to objects
+    """
+    assert local_dataset_with_annotations.id is not None
+    assert local_dataset_with_annotations.name is not None
+    assert local_dataset_with_annotations.directory is not None
+    result = run_cli_command(
+        f"darwin dataset push {local_dataset_with_annotations.name} {local_dataset_with_annotations.directory}"
+    )
+    assert result[0] == 0
+    sleep(2)
+    result = run_cli_command(
+        f"yes Y | darwin dataset import {local_dataset_with_annotations.name} darwin {Path(local_dataset_with_annotations.directory) / 'annotations'}"
+    )
+    assert result[0] == 0
+    
+    # Get class ids as export either needs a workflow and complete annotations or the class ids
+    url = f"{config_values.server}/api/teams/{config_values.team_slug}/annotation_classes?include_tags=true"
+    response = api_call('get', url, None, config_values.api_key)
+    if not response.ok:
+        raise Exception(f"Failed to get annotation classes: {response.text}")
+    classes = response.json()['annotation_classes']
+    class_ids = [c["id"] for c in classes]
+    class_str = " ".join([str(c) for c in class_ids])
+    # Test darwin export
+    sleep(2)
+    result = run_cli_command(
+        f"darwin dataset export {local_dataset_with_annotations.name} test_darwin_export --class-ids {class_str}"
+    )
+    assert result[0] == 0
 
 if __name__ == "__main__":
     pytest.main(["-vv", "-s", __file__])
