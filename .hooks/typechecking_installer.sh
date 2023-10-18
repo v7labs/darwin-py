884e5485e7f7db3427655bcc43d0ff8fe42f5cdc
Nathan Perkins <nathan@v7labs.com>
2023-10-18 14:22:45 +0100

    hook installer

---
+++ .hooks/typechecking_installer.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Usage
+# To install the typechecking linters, simply run the script in your terminal:
+
+# sh
+# This will install the linters and set up the pre-commit hook to run them on each changed Python file.
+# -o, --post-commit, -p, --pre-commit, default -p
+
+# Requirements
+# This script requires the following tools to be installed:
+
+# Git
+# Black
+# Ruff
+# Mypy
+# Make sure these tools are installed and available in your system's PATH before running the script.
+
+# Define the hook script
+HOOK_SCRIPT="#!/bin/bash
+# Get the list of changed Python files
+FILES=\$(git diff --name-only HEAD | grep '\.py$')
+
+# Run the linters on each changed file
+for FILE in \$FILES
+do
+    black \$FILE
+    ruff \$FILE
+    mypy \$FILE
+done
+"
+
+# Define the hook name
+HOOK_NAME="linters"
+
+# Define the hook directory
+HOOK_DIR="$(git rev-parse --show-toplevel)/.git/hooks"
+
+# Define the hook file names
+PRE_COMMIT_FILE="$HOOK_DIR/pre-commit"
+POST_COMMIT_FILE="$HOOK_DIR/post-commit"
+
+# Define the hook file names with the hook name
+PRE_COMMIT_HOOK_FILE="$HOOK_DIR/pre-commit-$HOOK_NAME"
+POST_COMMIT_HOOK_FILE="$HOOK_DIR/post-commit-$HOOK_NAME"
+
+# Define the default hook file name
+DEFAULT_HOOK_FILE="$POST_COMMIT_FILE"
+
+# Define the default hook type
+DEFAULT_HOOK_TYPE="post-commit"
+
+# Parse the command line arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -p|--pre-commit)
+        DEFAULT_HOOK_FILE="$PRE_COMMIT_FILE"
+        DEFAULT_HOOK_TYPE="pre-commit"
+        shift
+        ;;
+        -o|--post-commit)
+        DEFAULT_HOOK_FILE="$POST_COMMIT_FILE"
+        DEFAULT_HOOK_TYPE="post-commit"
+        shift
+        ;;
+        *)
+        echo "Unknown option: $key"
+        exit 1
+        ;;
+    esac
+done
+
+# Create the hook file
+echo "$HOOK_SCRIPT" > "$PRE_COMMIT_HOOK_FILE"
+echo "$HOOK_SCRIPT" > "$POST_COMMIT_HOOK_FILE"
+
+# Make the hook file executable
+chmod +x "$PRE_COMMIT_HOOK_FILE"
+chmod +x "$POST_COMMIT_HOOK_FILE"
+
+# Install the hook file
+if [ -f "$DEFAULT_HOOK_FILE" ]; then
+    mv "$DEFAULT_HOOK_FILE" "$HOOK_DIR/$DEFAULT_HOOK_TYPE-$(date +%s)-$HOOK_NAME"
+fi
+ln -s "$PRE_COMMIT_HOOK_FILE" "$DEFAULT_HOOK_FILE"
