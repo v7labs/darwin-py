8abc2dfd20745dcff1e2782b521a34523a83743f
Nathan Perkins <nathan@v7labs.com>
2023-10-18 17:46:17 +0100

    tests

--- darwin/future/tests/core/items/test_get_items.py
+++ darwin/future/tests/core/items/test_get_items.py
@@ -2,16 +2,19 @@ from typing import List
 from uuid import UUID, uuid4
 
 import responses
+from requests import get
 
 from darwin.future.core.client import ClientCore
 from darwin.future.core.items import get_item_ids, get_item_ids_stage
+from darwin.future.core.items.get import get_item, list_folders, list_items
+from darwin.future.core.types.common import QueryString
+from darwin.future.core.types.query import Query
+from darwin.future.data_objects.item import Folder, Item
 from darwin.future.tests.core.fixtures import *
 from darwin.future.tests.core.items.fixtures import *
 
 
-def test_get_item_ids(
-    UUIDs: List[UUID], UUIDs_str: List[str], base_client: ClientCore
-) -> None:
+def test_get_item_ids(UUIDs: List[UUID], UUIDs_str: List[str], base_client: ClientCore) -> None:
     with responses.RequestsMock() as rsps:
         rsps.add(
             rsps.GET,
@@ -24,9 +27,7 @@ def test_get_item_ids(
         assert item_ids == UUIDs
 
 
-def test_get_item_ids_stage(
-    UUIDs: List[UUID], UUIDs_str: List[str], base_client: ClientCore
-) -> None:
+def test_get_item_ids_stage(UUIDs: List[UUID], UUIDs_str: List[str], base_client: ClientCore) -> None:
     stage_id = str(uuid4())
     with responses.RequestsMock() as rsps:
         rsps.add(
@@ -38,3 +39,42 @@ def test_get_item_ids_stage(
         )
         item_ids = get_item_ids_stage(base_client, "default-team", "1337", stage_id)
         assert item_ids == UUIDs
+
+
+def test_get_item(base_items_json: List[dict], base_items: List[Item], base_client: ClientCore) -> None:
+    uuid = str(base_items[0].id)
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.GET,
+            base_client.config.api_endpoint + f"v2/teams/default-team/items/{uuid}",
+            json=base_items_json[0],
+            status=200,
+        )
+        item = get_item(base_client, "default-team", uuid)
+        assert item == base_items[0]
+
+
+def test_list_items(base_items_json: List[dict], base_items: List[Item], base_client: ClientCore) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.GET,
+            base_client.config.api_endpoint + f"v2/teams/default-team/items?dataset_ids=1337",
+            json={"items": base_items_json},
+            status=200,
+        )
+        items = list_items(base_client, "default-team", QueryString({"dataset_ids": "1337"}))
+        for item, comparator in zip(items, base_items):
+            assert item == comparator
+
+
+def test_list_folders(base_folders_json: List[dict], base_folders: List[Folder], base_client: ClientCore) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.GET,
+            base_client.config.api_endpoint + f"v2/teams/default-team/items/folders?dataset_ids=1337",
+            json={"folders": base_folders_json},
+            status=200,
+        )
+        folders = list_folders(base_client, "default-team", QueryString({"dataset_ids": "1337"}))
+        for folder, comparator in zip(folders, base_folders):
+            assert folder == comparator
