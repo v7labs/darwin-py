e4187a7d8bf21d22fea456e71d5bbe7249aa61cd
Owen <owen@owencjones.co.uk>
2023-10-24 13:15:20 +0100

    WIP: Trying out an approach

--- darwin/future/meta/objects/workflow.py
+++ darwin/future/meta/objects/workflow.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
+import logging
 from typing import List, Optional, Sequence, Union
 from uuid import UUID
 
-from darwin.cli_functions import upload_data
-from darwin.dataset.upload_manager import LocalFile
+from darwin.cli_functions import _load_client
+from darwin.dataset.remote_dataset_v2 import RemoteDatasetV2
+from darwin.dataset.upload_manager import LocalFile, UploadHandler
 from darwin.datatypes import PathLike
 from darwin.future.data_objects.workflow import WFDatasetCore, WFTypeCore, WorkflowCore
 from darwin.future.meta.objects.base import MetaBase
 from darwin.future.meta.queries.stage import StageQuery
+
+logger = logging.getLogger(__name__)
 
 
 class Workflow(MetaBase[WorkflowCore]):
@@ -52,24 +56,55 @@ class Workflow(MetaBase[WorkflowCore]):
         files_to_exclude: Optional[List[PathLike]] = None,
         fps: int = 1,
         path: Optional[str] = None,
-        frames: bool = False,
+        as_frames: bool = False,
         extract_views: bool = False,
         preserve_folders: bool = False,
         verbose: bool = False,
         auto_push: bool = True,
-    ) -> Workflow:
+    ) -> UploadHandler:
+        """ """
+
         assert self._element.dataset is not None
-        upload_data(
-            self.datasets[0].name,
-            files,  # type: ignore
-            files_to_exclude,
-            fps,
-            path,
-            frames,
-            extract_views,
-            preserve_folders,
-            verbose,
+        if verbose:
+            logger.warning(
+                "Verbose is deprecated in more recent versions, feedback on uploads can be obtained from returned object"
+            )
+
+        wf_dataset: WFDatasetCore = self._element.dataset
+
+        old_style_client = _load_client()
+        old_style_dataset = old_style_client.get_remote_dataset(wf_dataset.name)
+
+        assert isinstance(old_style_dataset, RemoteDatasetV2), "Only RemoteDatasetV2 is supported"
+
+        handler = old_style_dataset.push(
+            files,
+            blocking=False,
+            files_to_exclude=files_to_exclude,
+            fps=fps,
+            path=path,
+            as_frames=as_frames,
+            extract_views=extract_views,
+            preserve_folders=preserve_folders,
         )
-        if auto_push:
-            self.push_from_dataset_stage()
-        return self
+
+        # TODO: how do we handle autopush to next stage asynchrnously?
+
+        return handler
+
+        # if auto_push:
+        #     self.push_from_dataset_stage()
+        # return self
+
+    # async def _async_push(
+    #     self,
+    #     dataset: WFDatasetCore,
+    #     files: Sequence[Union[PathLike, LocalFile]],
+    #     files_to_exclude: Optional[List[PathLike]] = None,
+    #     fps: int = 1,
+    #     as_frames: bool = False,
+    #     extract_views: bool = False,
+    #     path: Optional[str] = None,
+    #     preserve_folders: bool = False,
+    # ):
+    #     assert self.client is not None
