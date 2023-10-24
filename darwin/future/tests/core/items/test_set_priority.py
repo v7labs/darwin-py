a5d34a1adc4a165db50acc42d9ff867763e28751
Owen <owen@owencjones.co.uk>
2023-10-24 11:03:44 +0100

    Improvements to tests

--- darwin/future/tests/core/items/test_set_priority.py
+++ darwin/future/tests/core/items/test_set_priority.py
@@ -1,53 +1,41 @@
 from unittest.mock import Mock
+from uuid import UUID
 
 import pytest
+import responses
+from sklearn import base
 
 from darwin.exceptions import DarwinException
 from darwin.future.core.client import ClientCore
-from darwin.future.core.items.set_priority import set_item_priority
+from darwin.future.core.items.set_item_priority import set_item_priority
+from darwin.future.tests.core.fixtures import *
 
 
-def test_set_item_priority() -> None:
-    # Create a mock API client
-    api_client = Mock(spec=ClientCore)
+@responses.activate
+def test_set_item_priority(base_client) -> None:
+    mock_response = responses.add(
+        responses.POST,
+        base_client.config.api_endpoint + "v2/teams/test-team/items/priority",
+        json={"affected_item_count": 0},
+    )
 
-    # Define the expected payload
-    expected_payload = {"priority": 10}
-
-    # Define the expected endpoint
-    expected_endpoint = "/v2/teams/test-team/items/priority"
-
-    # Define the expected response
-    expected_response = {"status": "success"}
-
-    # Configure the mock API client to return the expected response
-    api_client.post.return_value = expected_response
-
-    # Call the function being tested
     response = set_item_priority(
-        api_client=api_client,
-        team_slug="test-team",
-        priority=10,
+        base_client,
+        "test-team",
+        123,
+        UUID("00000000-0000-0000-0000-000000000000"),
+        999,
     )
 
-    # Verify that the API client was called with the expected arguments
-    api_client.post.assert_called_once_with(
-        endpoint=expected_endpoint,
-        data=expected_payload,
-    )
-
-    # Verify that the response matches the expected response
-    assert response == expected_response
+    assert response == {"affected_item_count": 0}
 
 
 def test_set_item_priority_with_filters() -> None:
-    # Create a mock API client
-    api_client = Mock(spec=ClientCore)
+    base_client = Mock(spec=ClientCore)
 
-    # Define the expected payload
     expected_payload = {
         "priority": 10,
-        "filters": {"status": "open"},
+        "filters": {"dataset_ids": [123], "item_ids": ["00000000-0000-0000-0000-000000000000"], "status": "open"},
     }
 
     # Define the expected endpoint
@@ -57,18 +45,20 @@ def test_set_item_priority_with_filters() -> None:
     expected_response = {"status": "success"}
 
     # Configure the mock API client to return the expected response
-    api_client.post.return_value = expected_response
+    base_client.post.return_value = expected_response
 
     # Call the function being tested
     response = set_item_priority(
-        api_client=api_client,
-        team_slug="test-team",
+        base_client,
+        "test-team",
+        123,
+        UUID("00000000-0000-0000-0000-000000000000"),
         priority=10,
         filters={"status": "open"},
     )
 
     # Verify that the API client was called with the expected arguments
-    api_client.post.assert_called_once_with(
+    base_client.post.assert_called_once_with(
         endpoint=expected_endpoint,
         data=expected_payload,
     )
@@ -89,5 +79,7 @@ def test_set_item_priority_with_error_response() -> None:
         set_item_priority(
             api_client=api_client,
             team_slug="test-team",
+            dataset_id=123,
+            item_id=UUID("00000000-0000-0000-0000-000000000000"),
             priority=10,
         )
