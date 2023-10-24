cfa9b924a9a33e8445a49c5331d6e83b142dcdda
Owen <owen@owencjones.co.uk>
2023-10-24 10:44:16 +0100

    Refactor to simplify

--- darwin/future/core/items/set_item_priority.py
+++ darwin/future/core/items/set_item_priority.py
@@ -1,4 +1,5 @@
 from typing import Dict, Optional
+from uuid import UUID
 
 from darwin.future.core.client import ClientCore
 from darwin.future.core.types.common import JSONType
@@ -8,8 +9,10 @@ from darwin.future.data_objects.typing import UnknownType
 def set_item_priority(
     api_client: ClientCore,
     team_slug: str,
+    dataset_id: int,
+    item_id: UUID,
     priority: int,
-    filters: Optional[Dict[str, UnknownType]] = None,
+    filters: Dict[str, UnknownType] = {},
 ) -> JSONType:
     """
     Sets the priority of an item
@@ -27,15 +30,14 @@ def set_item_priority(
     -------
     JSONType
     """
-    payload: Dict["str", UnknownType] = {
+    payload = {
         "priority": priority,
+        "filters": {
+            "item_ids": [str(item_id)],
+            "dataset_ids": [dataset_id],
+            **filters,
+        },
     }
-
-    if filters:
-        payload = {
-            **payload,
-            "filters": filters,
-        }
 
     return api_client.post(
         endpoint=f"/v2/teams/{team_slug}/items/priority",
