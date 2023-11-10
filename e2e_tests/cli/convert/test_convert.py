08d73cc34fd9eed8c9f4507349ee183e41805263
Owen <owen@owencjones.co.uk>
2023-11-10 16:21:27 +0000

    WIP basics of ticket in place

---
+++ e2e_tests/cli/convert/test_convert.py
@@ -0,0 +1,67 @@
+from os.path import dirname
+from pathlib import Path
+from typing import Tuple
+
+import pytest
+
+from e2e_tests.helpers import assert_cli, run_cli_command
+
+
+class TestYOLOv8SegmentedCli:
+    this_file_path = Path(dirname(__file__)).absolute()
+    data_path = (this_file_path / ".." / ".." / "data").resolve()
+
+    @pytest.fixture(autouse=True)
+    def config(self) -> None:
+        assert self.data_path.exists(), "Data path does not exist, tests cannot run"
+
+    def compare_directories(self, path: Path, expected_path: Path) -> Tuple[bool, str]:
+        """
+        Compare two directories recursively
+        """
+        assert path.exists() and expected_path.exists()
+        assert path.is_dir() and expected_path.is_dir()
+
+        for file in path.iterdir():
+            if file.is_dir():
+                # Recursively compare directories
+                result = self.compare_directories(file, expected_path / file.name)
+                if not result[0]:
+                    return result
+            else:
+                # Compare files
+                with open(file, "r") as f:
+                    content = f.read()
+
+                with open(expected_path / file.name, "r") as f:
+                    expected_content = f.read()
+
+                if content != expected_content:
+                    return (False, f"File {file} does not match expected file")
+
+        return (True, "")
+
+    @pytest.mark.parametrize(
+        "format, input_path, expectation_path",
+        [("yolo-segmented", data_path / "yolov8/from", data_path / "yolov8/to")],
+    )
+    def test_darwin_convert(self, format: str, input_path: Path, expectation_path: Path, tmp_path: Path) -> None:
+        """
+        Test converting a file format to another format
+        """
+        assert input_path is not None and expectation_path is not None, "Input or expectation path is None"
+        assert (
+            input_path.exists() and expectation_path.exists()
+        ), f"Input path {input_path.absolute()} or expectation path {expectation_path.absolute()} does not exist"
+        assert (
+            input_path.is_dir() and expectation_path.is_dir()
+        ), f"Input path {input_path.absolute()} or expectation path {expectation_path.absolute()} is not a directory"
+
+        result = run_cli_command(f"darwin dataset convert {str(input_path)} {format} -o {str(tmp_path)}")
+        assert_cli(result, 0)
+
+        assert self.compare_directories(expectation_path, tmp_path)[0]
+
+
+if __name__ == "__main__":
+    pytest.main(["-vv", "-s", __file__])
