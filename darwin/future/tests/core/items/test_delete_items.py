f1b8f3c844d27e168c6216b19e48ab4f0dd7ed6e
John Wilkie <124276291+JBWilkie@users.noreply.github.com>
2023-11-10 14:51:14 +0000

    [PY-560][external] Changes to core API methods to improve consistency (#712)
    * Changes to core API methods to improve consistency
    
    * Update darwin/future/core/items/archive_items.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Update darwin/future/core/items/delete_items.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Update darwin/future/core/items/move_items.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Update stage.py
    
    More descriptive variable names
    
    * Update darwin/future/core/items/move_items_to_folder.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Update darwin/future/core/items/restore_items.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Update darwin/future/core/items/set_item_priority.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Formatting fix
    
    ---------
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>

--- darwin/future/tests/core/items/test_delete_items.py
+++ darwin/future/tests/core/items/test_delete_items.py
@@ -1,54 +1,83 @@
-from unittest.mock import Mock
-from uuid import UUID
-
 import pytest
 import responses
 
-from darwin.exceptions import DarwinException
 from darwin.future.core.client import ClientCore
 from darwin.future.core.items.delete_items import delete_list_of_items
+from darwin.future.exceptions import BadRequest
 from darwin.future.tests.core.fixtures import *
 
 
 @responses.activate
 def test_delete_items_including_filters(base_client: ClientCore) -> None:
-    # Define the expected response
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {
+        "not_statuses": ["uploading", "annotate"],
+        "not_assignees": [123, 456, 789],
+        "item_ids": [
+            ("00000000-0000-0000-0000-000000000000"),
+            ("00000000-0000-0000-0000-000000000000"),
+        ],
+    }
+
     responses.add(
         responses.DELETE,
         base_client.config.api_endpoint + "v2/teams/test-team/items",
         json={"affected_item_count": 2},
+        status=200,
     )
 
-    # Call the function
     response = delete_list_of_items(
-        api_client=base_client,
-        team_slug="test-team",
-        dataset_id=000000,
-        item_ids=[
-            UUID("00000000-0000-0000-0000-000000000000"),
-            UUID("00000000-0000-0000-0000-000000000000"),
-        ],
-        filters={
-            "not_statuses": ["uploading", "annotate"],
-            "not_assignees": [123, 456, 789],
-        },
+        client=base_client,
+        team_slug=team_slug,
+        dataset_ids=dataset_ids,
+        filters=filters,
     )
 
-    # Check that the response mathces what we expect
     assert response == {"affected_item_count": 2}
 
 
-def test_delete_items_with_error_response() -> None:
-    api_client = Mock(spec=ClientCore)
-    api_client.delete.side_effect = DarwinException("Something went wrong")
+@responses.activate
+def test_delete_items_raises_on_incorrect_parameters(
+    base_client: ClientCore,
+) -> None:
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {}
 
-    with pytest.raises(DarwinException):
+    with pytest.raises(AssertionError):
         delete_list_of_items(
-            api_client=api_client,
-            team_slug="test-team",
-            dataset_id=000000,
-            item_ids=[
-                UUID("00000000-0000-0000-0000-000000000000"),
-                UUID("00000000-0000-0000-0000-000000000000"),
-            ],
+            client=base_client,
+            team_slug=team_slug,
+            dataset_ids=dataset_ids,
+            filters=filters,
+        )
+
+
+@responses.activate
+def test_delete_items_with_error_response(base_client: ClientCore) -> None:
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {
+        "not_statuses": ["uploading", "annotate"],
+        "not_assignees": [123, 456, 789],
+        "item_ids": [
+            ("00000000-0000-0000-0000-000000000000"),
+            ("00000000-0000-0000-0000-000000000000"),
+        ],
+    }
+
+    responses.add(
+        responses.DELETE,
+        base_client.config.api_endpoint + "v2/teams/test-team/items",
+        json={"error": "Bad Request"},
+        status=400,
+    )
+
+    with pytest.raises(BadRequest):
+        delete_list_of_items(
+            client=base_client,
+            team_slug=team_slug,
+            dataset_ids=dataset_ids,
+            filters=filters,
         )
--- darwin/future/tests/core/items/test_move_items_to_folder.py
+++ darwin/future/tests/core/items/test_move_items_to_folder.py
@@ -1,7 +1,5 @@
-from uuid import UUID
-
+import pytest
 import responses
-from pytest import raises
 
 from darwin.future.core.client import ClientCore
 from darwin.future.core.items.move_items_to_folder import move_list_of_items_to_folder
@@ -13,51 +11,83 @@ from darwin.future.tests.core.fixtures import *
 def test_move_list_of_items_to_folder_including_filters(
     base_client: ClientCore,
 ) -> None:
-    # Define the expected response
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {
+        "not_statuses": ["uploading", "annotate"],
+        "not_assignees": [123, 456, 789],
+        "item_ids": [
+            ("00000000-0000-0000-0000-000000000000"),
+            ("00000000-0000-0000-0000-000000000000"),
+        ],
+    }
+    path = "/test/path"
+
     responses.add(
         responses.POST,
         base_client.config.api_endpoint + "v2/teams/test-team/items/path",
         json={"affected_item_count": 2},
+        status=200,
     )
 
-    # Call the function
     response = move_list_of_items_to_folder(
-        api_client=base_client,
-        team_slug="test-team",
-        dataset_id=000000,
-        item_ids=[
-            UUID("00000000-0000-0000-0000-000000000000"),
-            UUID("00000000-0000-0000-0000-000000000000"),
-        ],
-        filters={
-            "not_statuses": ["uploading", "annotate"],
-            "not_assignees": [123, 456, 789],
-        },
-        path="/test/path",
+        client=base_client,
+        team_slug=team_slug,
+        dataset_ids=dataset_ids,
+        filters=filters,
+        path=path,
     )
 
-    # Check that the response mathces what we expect
     assert response == {"affected_item_count": 2}
 
 
-def test_move_list_of_items_to_folder_with_error_response(
+@responses.activate
+def test_move_list_of_items_to_folder_raises_on_incorrect_parameters(
     base_client: ClientCore,
 ) -> None:
-    with raises(BadRequest):
-        with responses.RequestsMock() as rsps:
-            rsps.add(
-                responses.POST,
-                base_client.config.api_endpoint + "v2/teams/test-team/items/path",
-                status=400,
-            )
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {}
+    path = "/test/path"
 
-            move_list_of_items_to_folder(
-                api_client=base_client,
-                team_slug="test-team",
-                dataset_id=000000,
-                item_ids=[
-                    UUID("00000000-0000-0000-0000-000000000000"),
-                    UUID("00000000-0000-0000-0000-000000000000"),
-                ],
-                path="/test/path",
-            )
+    with pytest.raises(AssertionError):
+        move_list_of_items_to_folder(
+            client=base_client,
+            team_slug=team_slug,
+            dataset_ids=dataset_ids,
+            path=path,
+            filters=filters,
+        )
+
+
+@responses.activate
+def test_move_list_of_items_to_folders_with_error_response(
+    base_client: ClientCore,
+) -> None:
+    dataset_ids = [1, 2, 3]
+    team_slug = "test-team"
+    filters = {
+        "not_statuses": ["uploading", "annotate"],
+        "not_assignees": [123, 456, 789],
+        "item_ids": [
+            ("00000000-0000-0000-0000-000000000000"),
+            ("00000000-0000-0000-0000-000000000000"),
+        ],
+    }
+    path = "/test/path"
+
+    responses.add(
+        responses.POST,
+        base_client.config.api_endpoint + "v2/teams/test-team/items/path",
+        json={"error": "Bad Request"},
+        status=400,
+    )
+
+    with pytest.raises(BadRequest):
+        move_list_of_items_to_folder(
+            client=base_client,
+            team_slug=team_slug,
+            dataset_ids=dataset_ids,
+            path=path,
+            filters=filters,
+        )
