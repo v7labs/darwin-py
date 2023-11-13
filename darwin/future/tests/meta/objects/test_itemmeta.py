5ab44b09ac5e9cbc5cec686a47d1ba741f25aa1c
John Wilkie <john@v7labs.com>
2023-11-13 21:35:54 +0000

    Added sad path tests

--- darwin/future/tests/meta/objects/test_itemmeta.py
+++ darwin/future/tests/meta/objects/test_itemmeta.py
@@ -1,9 +1,11 @@
 from uuid import UUID
 
+import pytest
 import responses
 from responses import json_params_matcher
 
 from darwin.future.data_objects.item import ItemLayout, ItemSlot
+from darwin.future.exceptions import BadRequest
 from darwin.future.meta.objects.item import Item
 from darwin.future.tests.meta.objects.fixtures import *
 
@@ -69,3 +71,29 @@ def test_move_to_folder(item: Item) -> None:
             json={},
         )
         item.move_to_folder(path)
+
+
+def test_move_to_folder_raises_on_incorrect_parameters(item: Item) -> None:
+    with responses.RequestsMock() as rsps:
+        team_slug = item.meta_params["team_slug"]
+        dataset_id = item.meta_params["dataset_id"]
+        path = 1234
+        rsps.add(
+            rsps.POST,
+            item.client.config.api_endpoint + f"v2/teams/{team_slug}/items/path",
+            status=400,
+            match=[
+                json_params_matcher(
+                    {
+                        "filters": {
+                            "item_ids": [str(item.id)],
+                            "dataset_ids": [dataset_id],
+                        },
+                        "path": path,
+                    }
+                )
+            ],
+            json={},
+        )
+        with pytest.raises(BadRequest):
+            item.move_to_folder(path)
