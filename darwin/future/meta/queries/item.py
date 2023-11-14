c638ce8db6b1991aa51f0cd56ac2cc7711f5b9fa
John Wilkie <124276291+JBWilkie@users.noreply.github.com>
2023-11-14 11:30:38 +0000

    [PY-400][external] Move Meta Item & ItemQuery to folder (#717)
    * Fixed missing path parameter in core move items to folder method
    
    * Fixed missing priority parameter in core set item priority method
    
    * Linting
    
    * Meta Item & ItemQuery functions to move between folders
    
    * Update darwin/future/tests/meta/queries/test_item.py
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>
    
    * Added sad path tests
    
    ---------
    
    Co-authored-by: Owen Jones <owencjones+github@gmail.com>

--- darwin/future/meta/queries/item.py
+++ darwin/future/meta/queries/item.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from darwin.future.core.items.delete_items import delete_list_of_items
 from darwin.future.core.items.get import list_items
+from darwin.future.core.items.move_items_to_folder import move_list_of_items_to_folder
 from darwin.future.core.types.common import QueryString
 from darwin.future.core.types.query import PaginatedQuery
 from darwin.future.meta.objects.item import Item
@@ -59,3 +60,24 @@ class ItemQuery(PaginatedQuery[Item]):
         ids = [item.id for item in self]
         filters = {"item_ids": [str(item) for item in ids]}
         delete_list_of_items(self.client, team_slug, dataset_ids, filters)
+
+    def move_to_folder(self, path) -> None:
+        if "team_slug" not in self.meta_params:
+            raise ValueError("Must specify team_slug to query items")
+        if (
+            "dataset_ids" not in self.meta_params
+            and "dataset_id" not in self.meta_params
+        ):
+            raise ValueError("Must specify dataset_ids to query items")
+        if not path:
+            raise ValueError("Must specify path to move items to")
+        dataset_ids = (
+            self.meta_params["dataset_ids"]
+            if "dataset_ids" in self.meta_params
+            else self.meta_params["dataset_id"]
+        )
+        team_slug = self.meta_params["team_slug"]
+        self.collect_all()
+        ids = [item.id for item in self]
+        filters = {"item_ids": [str(item) for item in ids]}
+        move_list_of_items_to_folder(self.client, team_slug, dataset_ids, path, filters)
