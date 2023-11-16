9b98d8ae5c72f2160c5e563b4dfae4a621d11e4b
John Wilkie <124276291+JBWilkie@users.noreply.github.com>
2023-11-16 13:37:32 +0000

    [PY-402][external] Archive Meta Item & ItemQuery functions (#719)
    * Meta functions & tests to archive items
    
    * Test improvements
    
    * Linting

--- darwin/future/meta/queries/item.py
+++ darwin/future/meta/queries/item.py
@@ -1,6 +1,7 @@
 from functools import reduce
 from typing import Dict
 
+from darwin.future.core.items.archive_items import archive_list_of_items
 from darwin.future.core.items.delete_items import delete_list_of_items
 from darwin.future.core.items.get import list_items
 from darwin.future.core.items.move_items_to_folder import move_list_of_items_to_folder
@@ -101,3 +102,22 @@ class ItemQuery(PaginatedQuery[Item]):
         ids = [item.id for item in self]
         filters = {"item_ids": [str(item) for item in ids]}
         restore_list_of_items(self.client, team_slug, dataset_ids, filters)
+
+    def archive(self) -> None:
+        if "team_slug" not in self.meta_params:
+            raise ValueError("Must specify team_slug to query items")
+        if (
+            "dataset_ids" not in self.meta_params
+            and "dataset_id" not in self.meta_params
+        ):
+            raise ValueError("Must specify dataset_ids to query items")
+        dataset_ids = (
+            self.meta_params["dataset_ids"]
+            if "dataset_ids" in self.meta_params
+            else self.meta_params["dataset_id"]
+        )
+        team_slug = self.meta_params["team_slug"]
+        self.collect_all()
+        ids = [item.id for item in self]
+        filters = {"item_ids": [str(item) for item in ids]}
+        archive_list_of_items(self.client, team_slug, dataset_ids, filters)
