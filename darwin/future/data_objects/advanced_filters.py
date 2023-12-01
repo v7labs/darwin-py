3b257fcb4bcc6139bb794bc8ada736229cc1e49f
Nathan Perkins <nathanjp91@gmail.com>
2023-12-01 12:30:54 +0000

    linting

--- darwin/future/data_objects/advanced_filters.py
+++ darwin/future/data_objects/advanced_filters.py
@@ -53,7 +53,7 @@ class Equals(BaseMatcher, GenericModel, Generic[T]):
 
 
 class DateRange(BaseMatcher):
-    name: Literal['date_range'] = "date_range"
+    name: Literal["date_range"] = "date_range"
     start: Optional[datetime] = None
     end: Optional[datetime] = None
 
