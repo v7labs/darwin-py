5e5ee6bb8cce8471fe12adc03aa7130706f8afec
John Wilkie <john@v7labs.com>
2023-12-11 18:48:11 +0000

    Changes to split (WIP)

--- tests/darwin/dataset/local_dataset_test.py
+++ tests/darwin/dataset/local_dataset_test.py
@@ -9,23 +9,6 @@ from tests.fixtures import *
 
 @pytest.mark.usefixtures("file_read_write_test")
 class TestBuildStems:
-    def test_look_into_annotations_directory_if_no_partition_specified(
-        self, team_dataset_release_path: Path, annotations_path: Path, split_path: Path
-    ):
-        (annotations_path / "1.json").mkdir()
-        (annotations_path / "2" / "2.json").mkdir(parents=True)
-        (annotations_path / "test" / "3" / "3.json").mkdir(parents=True)
-
-        stems = list(
-            get_annotation_filepaths(
-                team_dataset_release_path, annotations_path, "tag", split_path.name
-            )
-        )
-
-        assert "1" in stems
-        assert "2/2" in stems or "2\\2" in stems
-        assert "test/3/3" in stems or "test\\3\\3" in stems
-
     def test_raise_value_error_if_split_type_is_unknown(
         self, team_dataset_release_path: Path, annotations_path: Path, split_path: Path
     ):
