08173df8ca42fb249f87767b20a48d789d48ee49
Rafał Słota <rafal.s@v7labs.com>
2024-01-11 15:52:49 +0100

    Add assign batch action SDK (#765)
    * Add assign batch action SDK

--- darwin/future/meta/objects/item.py
+++ darwin/future/meta/objects/item.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Optional, Protocol, Union, cast
 from uuid import UUID
 
 from darwin.future.core.items.archive_items import archive_list_of_items
+from darwin.future.core.items.assign_items import assign_items
 from darwin.future.core.items.delete_items import delete_list_of_items
 from darwin.future.core.items.move_items_to_folder import move_list_of_items_to_folder
 from darwin.future.core.items.restore_items import restore_list_of_items
@@ -128,6 +129,31 @@ class Item(MetaBase[ItemCore]):
         dataset_id = cast(Union[int, List[int]], dataset_id)
         filters = {"item_ids": [str(self.id)]}
         set_item_layout(self.client, team_slug, dataset_id, layout, filters)
+
+    def assign(self, assignee_id: int, workflow_id: str | None = None) -> None:
+        if not assignee_id:
+            raise ValueError("Must specify assignee to assign items to")
+        if not workflow_id:
+            # if workflow_id is not specified, get it from the meta_params
+            # this will be present in the case of a workflow object
+            if "workflow_id" in self.meta_params:
+                workflow_id = str(self.meta_params["workflow_id"])
+            else:
+                raise ValueError("Must specify workflow_id to set items to")
+        assert isinstance(workflow_id, str)
+        team_slug, dataset_id = (
+            self.meta_params["team_slug"],
+            self.meta_params["dataset_id"]
+            if "dataset_id" in self.meta_params
+            else self.meta_params["dataset_ids"],
+        )
+        assert isinstance(team_slug, str)
+
+        dataset_id = cast(Union[int, List[int]], dataset_id)
+        filters = {"item_ids": [str(self.id)]}
+        assign_items(
+            self.client, team_slug, dataset_id, assignee_id, workflow_id, filters
+        )
 
     def tag(self, tag_id: int) -> None:
         team_slug, dataset_id = (
