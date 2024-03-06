65da6f12f3a8990b24a7a3fe9ca178fc56ca6d57
John Wilkie <john@v7labs.com>
2024-03-06 16:44:11 +0000

    Fixed misconfigured querystring filter name in fetch_remote_files()

--- test2.py
+++ test2.py
@@ -2,11 +2,11 @@ import darwin.importer as importer
 from darwin.client import Client
 from darwin.importer import get_importer
 
-DATASET_IDENTIFIER = "v7-john/bbox"
+DATASET_IDENTIFIER = "product-camera-team/xero-goat"
 FORMAT_NAME = "pascal_voc"
 ANNOTATION_PATHS = ["/Users/john/Desktop/pascal"]
 
-client = Client.local()
+client = Client.from_api_key("zCOGdus.PdY-kT07sKASoHsw8FmlczMRDKw532Uz")
 dataset = client.get_remote_dataset(dataset_identifier=DATASET_IDENTIFIER)
 parser = get_importer(FORMAT_NAME)
-importer.import_annotations(dataset, parser, ANNOTATION_PATHS, append=True)
+importer.import_annotations(dataset, parser, ANNOTATION_PATHS, append=False)
