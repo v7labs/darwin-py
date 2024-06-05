d3b8e94c1ba3377162b0b75ef2ae5767143a8a40
John Wilkie <john@v7labs.com>
2024-06-05 13:58:26 +0100

    Added tests

--- tests/darwin/exporter/formats/export_nifti_test.py
+++ tests/darwin/exporter/formats/export_nifti_test.py
@@ -1,5 +1,6 @@
 import tempfile
 from pathlib import Path
+from unittest.mock import patch
 from zipfile import ZipFile
 
 import nibabel as nib
@@ -77,7 +78,7 @@ def test_video_annotation_nifti_export_mpr(team_slug_darwin_json_v2: str):
             video_annotations = list(
                 darwin_to_dt_gen(video_annotation_filepaths, False)
             )
-            nifti.export(video_annotations, output_dir=tmpdir)
+            nifti.export(video_annotations, output_dir=Path(tmpdir))
             export_im = nib.load(
                 annotations_dir / "hippocampus_001_mpr_1_test_hippo.nii.gz"
             ).get_fdata()
@@ -85,3 +86,47 @@ def test_video_annotation_nifti_export_mpr(team_slug_darwin_json_v2: str):
                 annotations_dir / "hippocampus_001_mpr_1_test_hippo.nii.gz"
             ).get_fdata()
             assert np.allclose(export_im, expected_im)
+
+
+def test_export_calls_populate_output_volumes_from_polygons(
+    team_slug_darwin_json_v2: str,
+):
+    with patch(
+        "darwin.exporter.formats.nifti.populate_output_volumes_from_polygons"
+    ) as mock:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with ZipFile("tests/data.zip") as zfile:
+                zfile.extractall(tmpdir)
+            annotations_dir = (
+                Path(tmpdir)
+                / team_slug_darwin_json_v2
+                / "nifti/releases/latest/annotations"
+            )
+            video_annotation_filepaths = [annotations_dir / "polygon_no_mask.json"]
+            video_annotations = list(
+                darwin_to_dt_gen(video_annotation_filepaths, False)
+            )
+            nifti.export(video_annotations, output_dir=Path(tmpdir))
+            mock.assert_called()
+
+
+def test_export_calls_populate_output_volumes_from_raster_layer(
+    team_slug_darwin_json_v2: str,
+):
+    with patch(
+        "darwin.exporter.formats.nifti.populate_output_volumes_from_raster_layer"
+    ) as mock:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with ZipFile("tests/data.zip") as zfile:
+                zfile.extractall(tmpdir)
+            annotations_dir = (
+                Path(tmpdir)
+                / team_slug_darwin_json_v2
+                / "nifti/releases/latest/annotations"
+            )
+            video_annotation_filepaths = [annotations_dir / "mask_no_polygon.json"]
+            video_annotations = list(
+                darwin_to_dt_gen(video_annotation_filepaths, False)
+            )
+            nifti.export(video_annotations, output_dir=Path(tmpdir))
+            mock.assert_called()
