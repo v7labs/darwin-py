c39039cb47c0fbaa1b775540b9c5589ea7ef1d1f
John Wilkie <john@v7labs.com>
2024-06-05 14:26:55 +0100

    Docstring updates

--- darwin/exporter/formats/nifti.py
+++ darwin/exporter/formats/nifti.py
@@ -295,23 +295,17 @@ def populate_output_volumes_from_polygons(
     output_volumes: Dict,
 ) -> Dict:
     """
-    Exports the given ``AnnotationFile``\\s into nifti format inside of the given
-    ``output_dir``. Deletes everything within ``output_dir/masks`` before writting to it.
+    Populates the output volumes with the given polygon annotations. The annotations are converted into masks
+    and added to the corresponding volume based on the series instance UID.
 
     Parameters
     ----------
-    annotation : Union[dt.Annotation, dt.VideoAnnotation]
-        The Union of these two files used to populate the volume with
+    annotations : List[Union[dt.Annotation, dt.VideoAnnotation]]
+        List of polygon annotations used to populate the volume with
     slot_map : Dict
         Dictionary of the different slots within the annotation file
     output_volumes : Dict
-        volumes created from the build_output_volumes file
-
-    Returns
-    -------
-    volume : dict
-        Returns dict of volumes with class names as keys and volumes as values
-
+        Volumes created from the build_output_volumes file
     """
     for annotation in annotations:
         slot_name = annotation.slot_names[0]
