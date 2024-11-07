2e12fe7c333570065f6aeb02f9aca4f74d940fba
John Wilkie <john@v7labs.com>
2024-11-07 13:06:01 +0000

    Test coverage

--- tests/darwin/importer/formats/import_nifti_test.py
+++ tests/darwin/importer/formats/import_nifti_test.py
@@ -19,6 +19,7 @@ from darwin.datatypes import (
 )
 from darwin.importer.formats.nifti import get_new_axial_size, parse_path
 from tests.fixtures import *
+from darwin.utils.utils import parse_darwin_json
 
 
 def test_image_annotation_nifti_import_single_slot(team_slug_darwin_json_v2: str):
@@ -389,3 +390,108 @@ if __name__ == "__main__":
             "w",
         ) as f:
             f.write(output_json_string)
+
+
+def adjust_nifti_label_filepath(nifti_annotation_filepath: Path, nifti_filepath: Path):
+    """
+    Adjusts a specific NifTI label path to point to a local NifTI file for import testing.
+    This is requied to allow the test to run in multiple environments
+    """
+    with open(nifti_annotation_filepath) as f:
+        input_data = json.load(f)
+
+    # Inject the nifti_filepath into the data.label key
+    input_data["data"][0]["label"] = str(nifti_filepath)
+
+    # Save the modified JSON back to the file
+    with open(nifti_annotation_filepath, "w") as f:
+        json.dump(input_data, f, indent=4)
+
+
+def round_polygon_annotation_coordinates(annotation, decimal_places=2):
+    """
+    Rounds all coordinates in the annotation to a specified number of decimal places.
+
+    Parameters:
+    - annotation: The annotation data (list of lists).
+    - decimal_places: The number of decimal places to round to.
+
+    Returns:
+    - A new annotation structure with rounded coordinates.
+    """
+    return [
+        [
+            {
+                "x": round(point["x"], decimal_places),
+                "y": round(point["y"], decimal_places),
+            }
+            for point in path
+        ]
+        for path in annotation
+    ]
+
+
+def test_parse_path_nifti_with_legacy_scaling():
+    nifti_annotation_filepath = (
+        Path(__file__).parents[2] / "data" / "nifti" / "nifti.json"
+    )
+    nifti_filepath = (
+        Path(__file__).parents[2] / "data" / "nifti" / "BRAINIX_NIFTI_ROI.nii.gz"
+    )
+    expected_annotations_filepath = (
+        Path(__file__).parents[2]
+        / "data"
+        / "nifti"
+        / "legacy"
+        / "BRAINIX_NIFTI_ROI.nii.json"
+    )
+    adjust_nifti_label_filepath(nifti_annotation_filepath, nifti_filepath)
+    expected_annotations = parse_darwin_json(expected_annotations_filepath)
+    parsed_annotations = parse_path(nifti_annotation_filepath, legacy=True)
+    for frame_idx in expected_annotations.annotations[0].frames:
+        expected_annotation = (
+            expected_annotations.annotations[0].frames[frame_idx].data["paths"]
+        )
+        parsed_annotation = (
+            parsed_annotations[0].annotations[0].frames[frame_idx].data["paths"]
+        )
+        expected_annotation_rounded = round_polygon_annotation_coordinates(
+            expected_annotation, decimal_places=4
+        )
+        parsed_annotation_rounded = round_polygon_annotation_coordinates(
+            parsed_annotation, decimal_places=4
+        )
+        assert expected_annotation_rounded == parsed_annotation_rounded
+
+
+def test_parse_path_nifti_without_legacy_scaling():
+    nifti_annotation_filepath = (
+        Path(__file__).parents[2] / "data" / "nifti" / "nifti.json"
+    )
+    nifti_filepath = (
+        Path(__file__).parents[2] / "data" / "nifti" / "BRAINIX_NIFTI_ROI.nii.gz"
+    )
+    expected_annotations_filepath = (
+        Path(__file__).parents[2]
+        / "data"
+        / "nifti"
+        / "no-legacy"
+        / "BRAINIX_NIFTI_ROI.nii.json"
+    )
+    adjust_nifti_label_filepath(nifti_annotation_filepath, nifti_filepath)
+    expected_annotations = parse_darwin_json(expected_annotations_filepath)
+    parsed_annotations = parse_path(nifti_annotation_filepath, legacy=False)
+    for frame_idx in expected_annotations.annotations[0].frames:
+        expected_annotation = (
+            expected_annotations.annotations[0].frames[frame_idx].data["paths"]
+        )
+        parsed_annotation = (
+            parsed_annotations[0].annotations[0].frames[frame_idx].data["paths"]
+        )
+        expected_annotation_rounded = round_polygon_annotation_coordinates(
+            expected_annotation, decimal_places=4
+        )
+        parsed_annotation_rounded = round_polygon_annotation_coordinates(
+            parsed_annotation, decimal_places=4
+        )
+        assert expected_annotation_rounded == parsed_annotation_rounded
